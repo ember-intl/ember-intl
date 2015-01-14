cbab2df65b3a946268779954f231b09cd4e864e8
Jason Mitchell <jasonmit@yahoo-inc.com>
2015-01-13 19:03:53 -0800

    [Enhancement] Adding format-html-message fixes #12

--- addon/components/format-html-message.js
+++ addon/components/format-html-message.js
@@ -1,31 +1,27 @@
-/* {{formatMessage message
-			product='Mac Mini'
-			price='2000.0015'
-			deadline=1390518044403}}
-
-	Properties need to be assumed as user input
-	and should be escaped.
-*/
-import IntlComponent from './main';
+import IntlComponent from './format-message';
 import escape from '../utils/escape';
+
+function escapeProps (props) {
+	return Object.keys(props).reduce(function (escapedProps, name) {
+		var value = props[name];
+
+		// TODO: Can we force string coersion here? Or would that not be needed
+		// and possible mess with IntlMessageFormat?
+		if (typeof value === 'string') {
+			value = escape(value);
+		}
+
+		escapedProps[name] = value;
+		return escapedProps;
+	}, {});
+}
 
 var FormatHTMLMessageComponent = IntlComponent.extend({
 	tagName:           'format-html-message',
 	instrumentDisplay: '{{format-html-message}}',
 
-	escapeProps: function (props) {
-		return Object.keys(props).reduce(function (escapedProps, name) {
-			var value = props[name];
-
-			// TODO: Can we force string coersion here? Or would that not be needed
-			// and possible mess with IntlMessageFormat?
-			if (typeof value === 'string') {
-				value = escape(value);
-			}
-
-			escapedProps[name] = value;
-			return escapedProps;
-		}, {});
+	renderer: function (props, options) {
+		return this.get('intl').formatMessage(props.value, escapeProps(this.get('pojoContext')), options);
 	}
 });
 
--- tests/dummy/app/messages/en.js
+++ tests/dummy/app/messages/en.js
@@ -1,5 +1,8 @@
 export default {
 	product: {
-		info: '{product} will cost {price, number, EUR} if ordered by {deadline, date, time}'
+		info: '{product} will cost {price, number, EUR} if ordered by {deadline, date, time}',
+		html: {
+			info: '<strong>{product}</strong> will cost <em>{price, number, EUR}</em> if ordered by {deadline, date, time}'
+		}
 	}
 };
