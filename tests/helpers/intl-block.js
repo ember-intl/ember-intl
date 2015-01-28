a58ebae454461ff2a6548506c5cacf64173d594a
Jason Mitchell <jasonmit@yahoo-inc.com>
2015-01-27 17:04:52 -0800

    Adding helpers to intl-block

--- tests/helpers/intl-block.js
+++ tests/helpers/intl-block.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 import IntlService from '../../service/intl';
+
 import FormatNumber from 'ember-intl/formatters/format-number';
 import formatNumberHelper from '../../helpers/format-number';
+
+import FormatDate from 'ember-intl/formatters/format-date';
+import formatDateHelper from '../../helpers/format-date';
+
+import FormatTime from 'ember-intl/formatters/format-time';
+import formatTimeHelper from '../../helpers/format-time';
+
+import FormatRelative from 'ember-intl/formatters/format-relative';
+import formatRelativeHelper from '../../helpers/format-relative';
+
 import { runAppend, runDestroy } from '../helpers/run-append';
 
 export default function () {
@@ -28,6 +39,16 @@ export default function () {
 				container.injection('ember-intl@formatter', 'intl', 'intl:main');
 				container.register('ember-intl@formatter:format-number', FormatNumber);
 				container.register('helper:format-number', formatNumberHelper, { instantiate: false });
+
+				container.register('ember-intl@formatter:format-date', FormatDate);
+				container.register('helper:format-date', formatDateHelper, { instantiate: false });
+
+				container.register('ember-intl@formatter:format-time', FormatTime);
+				container.register('helper:format-time', formatTimeHelper, { instantiate: false });
+
+				container.register('ember-intl@formatter:format-relative', FormatRelative);
+				container.register('helper:format-relative', formatRelativeHelper, { instantiate: false });
+
 				container.register('component-lookup:main', Ember.Object.extend({
 					lookupFactory: function () { }
 				}));
