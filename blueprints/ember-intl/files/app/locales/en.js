23065bf4b93a6090a370ded852d57de1821ef2b5
Gabor Babicz <gabor.babicz@gmail.com>
2015-02-05 14:00:02 +0100

    Blueprints should match ember-cli’s 2-space indent convention

--- blueprints/ember-intl/files/app/locales/en.js
+++ blueprints/ember-intl/files/app/locales/en.js
@@ -1,11 +1,11 @@
 export default {
-    locale: "en",
-    messages: {
-        product: {
-            info: '{product} will cost {price, number, EUR} if ordered by {deadline, date, time}',
-            html: {
-                info: '<strong>{product}</strong> will cost <em>{price, number, EUR}</em> if ordered by {deadline, date, time}'
-            }
-        }
+  locale: "en",
+  messages: {
+    product: {
+      info: '{product} will cost {price, number, EUR} if ordered by {deadline, date, time}',
+      html: {
+        info: '<strong>{product}</strong> will cost <em>{price, number, EUR}</em> if ordered by {deadline, date, time}'
+      }
     }
+  }
 };
