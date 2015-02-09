3c8e8bb75414d94a01e4be080514a096cf1d71a1
Gabor Babicz <gabor.babicz@gmail.com>
2015-02-09 08:48:27 +0100

    Smoke test

---
+++ tests/dummy/app/controllers/smoke.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+var Controller = Ember.Controller;
+var computed = Ember.computed;
+
+var now       = new Date();
+var yesterday = new Date(now).setDate(now.getDate() - 1);
+
+
+export default Controller.extend({
+  locales:    ['en-US', 'fr-FR', 'es'],
+  num:        1000,
+  yesterday:  yesterday,
+
+  currentLocale: computed('intl.locales', function (key, value) {
+    if (arguments.length === 2) {
+      this.set('intl.locales', arguments[1]);
+    }
+
+    return this.get('intl.locales.firstObject') || this.get('intl.locales');
+  })
+});
