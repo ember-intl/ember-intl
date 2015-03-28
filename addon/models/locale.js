670156d534481e7a332dd986f1908429064adcfb
Jason Mitchell <jasonmit@yahoo-inc.com>
2015-03-27 18:28:16 -0700

    deprecating models/locale

---
+++ addon/models/locale.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import Translation from './translation';
+
+export default Translation.extend({
+    init: function () {
+        Ember.Logger.warn('`ember-intl/models/locale` is deprecated in favor of `ember-intl/models/translation`');
+    }
+});
