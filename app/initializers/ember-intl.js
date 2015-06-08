ad3c8337a5a24f103fa46f2ecc2d5a6ed34d3163
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-06-07 17:13:04 -0700

    instance initializer fallback for < 1.12 users

--- app/initializers/ember-intl.js
+++ app/initializers/ember-intl.js
@@ -4,7 +4,10 @@
  */
 
 import Ember from 'ember';
+
 import ENV from '../config/environment';
+import { initializeIntl } from '../instance-initializers/ember-intl';
+
 import { addLocaleData } from 'ember-intl/utils/data';
 import { filterBy } from 'ember-intl/utils/initialize';
 import FormatDate from 'ember-intl/helpers/format-date';
@@ -15,49 +18,11 @@ import FormatHtmlMessage from 'ember-intl/helpers/format-html-message';
 import FormatMessage from 'ember-intl/helpers/format-message';
 import IntlAdapter from 'ember-intl/adapters/-intl-adapter';
 
-export var registerIntl = function (container, service) {
-    if (!service) {
-        service = container.lookup('service:intl');
-    }
-
-    filterBy(ENV, 'cldrs').forEach((key) => {
-        addLocaleData(require(key, null, null, true)['default']);
-    });
-
-    filterBy(ENV, 'translations').forEach((key) => {
-        let localeSplit = key.split('\/');
-        let locale = localeSplit[localeSplit.length - 1];
-        service.createLocale(locale, require(key, null, null, true)['default']);
-    });
-
-    container.optionsForType('formats', {
-        singleton:   true,
-        instantiate: false
-    });
-
-    if (!container.has('adapter:-intl-adapter')) {
-        container.register('adapter:-intl-adapter', IntlAdapter);
-    }
-
-    // only here for backwards compat.
-    container.register('intl:main', container.lookup('service:intl'), {
-        instantiate: false,
-        singleton:   true
-    });
-
-    Ember.HTMLBars._registerHelper('format-date', FormatDate);
-    Ember.HTMLBars._registerHelper('format-time', FormatTime);
-    Ember.HTMLBars._registerHelper('format-relative', FormatRelative);
-    Ember.HTMLBars._registerHelper('format-number', FormatNumber);
-    Ember.HTMLBars._registerHelper('format-html-message', FormatHtmlMessage);
-    Ember.HTMLBars._registerHelper('format-message', FormatMessage);
-}
-
 export default {
     name: 'ember-intl',
 
-    initialize(registry, application) {
-        application.intl = registry.lookup('service:intl');
+    initialize(registry, app) {
+        app.intl = registry.lookup('service:intl');
 
         registry.optionsForType('formats', {
             singleton:   true,
@@ -69,10 +34,12 @@ export default {
         }
 
         // only here for backwards compat.
-        registry.register('intl:main', registry.lookup('service:intl'), {
-            instantiate: false,
-            singleton:   true
-        });
+        if (!registry.has('intl:main')) {
+            registry.register('intl:main', registry.lookup('service:intl'), {
+                instantiate: false,
+                singleton:   true
+            });
+        }
 
         Ember.HTMLBars._registerHelper('format-date', FormatDate);
         Ember.HTMLBars._registerHelper('format-time', FormatTime);
@@ -80,5 +47,11 @@ export default {
         Ember.HTMLBars._registerHelper('format-number', FormatNumber);
         Ember.HTMLBars._registerHelper('format-html-message', FormatHtmlMessage);
         Ember.HTMLBars._registerHelper('format-message', FormatMessage);
+
+        if (app.instanceInitializer) {
+            return;
+        }
+
+        initializeIntl(registry, app);
     }
 };
