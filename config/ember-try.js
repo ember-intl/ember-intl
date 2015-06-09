52fec5df78aaf8f4830507c4ee03d5b281adb3a6
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-06-09 01:38:47 -0700

    adding ember try, 1.10 and 1.11 are failing though

---
+++ config/ember-try.js
@@ -0,0 +1,22 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'Ember 1.10',
+      dependencies: {
+        'ember': '1.10.0'
+      }
+    },
+		{
+      name: 'Ember 1.11',
+      dependencies: {
+        'ember': '1.11'
+      }
+    },
+		{
+      name: 'Ember 1.12',
+      dependencies: {
+        'ember': '1.12'
+      }
+    }
+  ]
+};
