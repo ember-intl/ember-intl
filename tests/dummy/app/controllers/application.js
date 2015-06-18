46d9351f86142f981ef9a2b84248aedeb97c9d2a
Jason Mitchell <jasonmit@yahoo-inc.com>
2015-06-18 15:51:56 -0700

    Removing deprecation warnings for 1.13 tests

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -5,13 +5,15 @@ export default Ember.Controller.extend({
     intl:    Ember.inject.service(),
     options: Ember.A(['en-US', 'fr-FR', 'es']),
 
+    actions: {
+        changeLocale: function(localeName) {
+            this.set('intl.locale', localeName);
+        }
+    },
+
     value: computed('intl.locale', {
-      get() {
-        return this.get('intl.locale.firstObject') || this.get('intl.locale');
-      },
-      set(key, value) {
-        this.set('intl.locale', value);
-        return this.get('intl.locale.firstObject') || this.get('intl.locale');
-      }
+        get() {
+            return this.get('intl.locale.firstObject') || this.get('intl.locale');
+        }
     })
 });
