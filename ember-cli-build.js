1f14b14e7d5fdf74b89c7cabe615964e7db739a8
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-30 17:18:18 -0700

    removing Brocfile

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,6 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
+var path = require('path');
 
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
