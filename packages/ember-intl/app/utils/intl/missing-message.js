6b057dcdbe0cc56d5c7d4f5ae9895753bdeb957a
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-01-25 00:09:50 -0800

    BUGFIX: warn on unset locale #280

--- packages/ember-intl/app/utils/intl/missing-message.js
+++ packages/ember-intl/app/utils/intl/missing-message.js
@@ -3,7 +3,12 @@ import Ember from 'ember';
 const { Logger:logger } = Ember;
 
 export default function missingMessage(key, locales) {
-  logger.warn(`translation: '${key}' on locale: '${locales.join(', ')}' was not found.`);
+  if (!locales) {
+    logger.warn('ember-intl: no locale has been set. https://github.com/yahoo/ember-intl#configure-application-wide-locale')
+  }
+  else {
+    logger.warn(`ember-intl: translation: '${key}' on locale: '${locales.join(', ')}' was not found.`);
+  }
 
   return `Missing translation: ${key}`;
 }
