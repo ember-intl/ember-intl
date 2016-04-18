5a82b8b6d1278977189208dcba08542b4da68912
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-04-17 17:26:57 -0700

    Upgrading deps

--- packages/ember-intl/ember-cli-build.js
+++ packages/ember-intl/ember-cli-build.js
@@ -3,7 +3,6 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
-var path = require('path');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -13,7 +12,7 @@ module.exports = function(defaults) {
   app.options.snippetPaths = ['tests/dummy/snippets'];
 
   /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
