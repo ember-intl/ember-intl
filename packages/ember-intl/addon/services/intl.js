f358dfbecb2fedfd6ef9496f034dbb27251ae3bf
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-01-15 14:16:01 -0800

    Do not merge options into the format object

--- packages/ember-intl/addon/services/intl.js
+++ packages/ember-intl/addon/services/intl.js
@@ -40,7 +40,7 @@ function formatterProxy(formatType) {
     }
 
     if (typeof options.format === 'string') {
-      options = assign(this.getFormat(formatType, options.format), options);
+      options = assign({}, this.getFormat(formatType, options.format), options);
     }
 
     let formatter = this._owner.lookup(`ember-intl@formatter:format-${formatType}`);
