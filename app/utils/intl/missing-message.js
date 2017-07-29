74cc41bbca03d3b15dcbab9a2828297e34d6b4af
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-07-29 03:02:56 -0700

    Reexporting from addon folder

--- app/utils/intl/missing-message.js
+++ app/utils/intl/missing-message.js
@@ -1,18 +1,1 @@
-import Ember from 'ember';
-import links from 'ember-intl/utils/links';
-
-const { warn } = Ember;
-
-export default function missingMessage(key, locales) {
-  if (!locales) {
-    warn(`[ember-intl] no locale has been set. Documentation: ${links.unsetLocale}`, false, {
-      id: 'ember-intl-no-locale-set'
-    });
-  } else {
-    warn(`[ember-intl] translation: '${key}' on locale: '${locales.join(', ')}' was not found.`, false, {
-      id: 'ember-intl-missing-translation'
-    });
-  }
-
-  return `Missing translation: ${key}`;
-}
+export { default } from 'ember-intl/utils/missing-message';
