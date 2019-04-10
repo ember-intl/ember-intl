c515f83d15a78374708aa6c10dd0a3b4ba756dd0
Jason Mitchell <jason.mitchell.w@gmail.com>
2019-04-09 20:59:53 -0700

    chore: Remove obsolete initializer. (#845)

--- app/instance-initializers/ember-intl.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2015, Yahoo! Inc.
- * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
- */
-
-import initializer, { instanceInitializer } from 'ember-intl/initializer';
-
-export { instanceInitializer };
-export default initializer;
