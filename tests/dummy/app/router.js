df83cccdfb610cdb5fa2840a1e65f9f6beaf9dcf
fredericsoumare <frederic.soumare@gmail.comfrederic.soumare@qonto.eu>
2019-04-10 13:37:14 +0200

    add docs routes and skeleton

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,15 @@
-import EmberRouter from '@ember/routing/router';
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
+const Router = AddonDocsRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
+  docsRoute(this, function() {
+    this.route('usage')
+  });
   this.route('smoke');
 });
 
