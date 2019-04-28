533fc125bf7ad3f41276d877bcc2016d85aad6e1
Fred ST <frederic.soumare@gmail.com>
2019-04-28 20:48:11 +0200

    fix breaking acceptance test

---
+++ tests/dummy/app/pods/smoke/controller.js
@@ -0,0 +1,18 @@
+import { A } from '@ember/array';
+import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
+
+const now = new Date();
+const yesterday = new Date(now).setDate(now.getDate() - 1);
+
+export default Controller.extend({
+  intl: service(),
+  locales: A(['en-us', 'fr-fr', 'es-es']),
+  num: 1000,
+  yesterday: yesterday,
+  actions: {
+    changeLocale(locale) {
+      this.get('intl').set('locale', locale);
+    }
+  }
+});
