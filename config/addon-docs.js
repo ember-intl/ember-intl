056f9ad2402154aaa432504accaaadfeb630ea67
Jason Mitchell <jason.mitchell.w@gmail.com>
2019-04-29 12:21:50 -0700

    chore: removing deploy run script

--- config/addon-docs.js
+++ config/addon-docs.js
@@ -6,7 +6,4 @@ const AddonDocsConfig = require('ember-cli-addon-docs/lib/config');
 module.exports = class extends AddonDocsConfig {
   // See https://ember-learn.github.io/ember-cli-addon-docs/docs/deploying
   // for details on configuration you can override here.
-  getRootURL() {
-    return '/';
-  }
 };
