4803fb4cbc4d2d7ea4f80675ac6b95a2d265d7f2
David Arias Vázquez <david@ariasbros.com>
2019-08-12 20:16:45 +0200

    fix(translations): Add support to old Ember versions

--- tests/dummy/app/pods/smoke/controller.js
+++ tests/dummy/app/pods/smoke/controller.js
@@ -1,6 +1,7 @@
 import { A } from '@ember/array';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 const now = new Date();
 const yesterday = new Date(now).setDate(now.getDate() - 1);
@@ -16,7 +17,7 @@ export default Controller.extend({
     }
   },
 
-  get namespacesAreActive() {
-    return this.intl.exists('subdirectory.smoke.subdirectory');
-  }
+  namespacesAreActive: computed(function() {
+    return this.get('intl').exists('subdirectory.smoke.subdirectory');
+  })
 });
