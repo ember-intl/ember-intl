16654eef871862f8e436d412d9416c60d0fc1bd2
David Arias Vázquez <david@ariasbros.com>
2019-08-12 20:28:58 +0200

    build(translations): Add tests for wrapWithNamespaceIfNeeded function

---
+++ tests-node/unit/broccoli/translation-reducer/wrap-with-namespace-if-needed.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+'use strict';
+
+let expect = require('chai').expect;
+
+let wrapWithNamespaceIfNeeded = require('../../../../lib/broccoli/translation-reducer/utils/wrap-with-namespace-if-needed');
+
+describe('wrapWithNamespaceIfNeeded', function() {
+  [
+    [{ a: true, b: false }, { a: true, b: false }, "/tmp/broccoli_debug-output_path-l4iBcmcT/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", []],
+    [{ a: true, b: false }, { a: true, b: false }, "/tmp/broccoli_debug-output_path-l4iBcmcT/an-addon/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", ["an-addon", "other-addon"]],
+    [{ a: true, b: false }, { a: true, b: false }, "/tmp/broccoli_debug-output_path-l4iBcmcT/@a-scoped/addon/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", ["@a-scoped/addon", "an-addon", "other-addon"]],
+    [{ a: true, b: false }, { foo: { a: true, b: false } }, "/tmp/broccoli_debug-output_path-l4iBcmcT/foo/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", []],
+    [{ a: true, b: false }, { foo: { bar: { a: true, b: false } } }, "/tmp/broccoli_debug-output_path-l4iBcmcT/foo/bar/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", []],
+    [{ a: true, b: false }, { foo: { bar: { a: true, b: false } } }, "/tmp/broccoli_debug-output_path-l4iBcmcT/an-addon/foo/bar/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", ["an-addon", "other-addon"]],
+    [{ a: true, b: false }, { foo: { bar: { a: true, b: false } } }, "/tmp/broccoli_debug-output_path-l4iBcmcT/an-addon/foo/bar/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", ["other-addon", "an-addon"]],
+    [{ a: true, b: false }, { foo: { bar: { a: true, b: false } } }, "/tmp/broccoli_debug-output_path-l4iBcmcT/@a-scoped/addon/foo/bar/en-us.json", "/tmp/broccoli_debug-output_path-l4iBcmcT", ["@a-scoped/addon", "other-addon", "an-addon"]],
+  ].forEach(([object, expected, filepath, inputPath, addonNames]) => {
+    it(`${JSON.stringify(object)} -> ${JSON.stringify(expected)}`, function() {
+      expect(wrapWithNamespaceIfNeeded(object, filepath, inputPath, addonNames)).to.deep.equal(expected);
+    });
+  });
+});
