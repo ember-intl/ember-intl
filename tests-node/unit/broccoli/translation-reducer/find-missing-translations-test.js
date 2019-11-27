cf6df24178d367527a8b25d3a5a19759bd185e3f
Jason Mitchell <jason.mitchell.w@gmail.com>
2019-11-26 21:58:40 -0800

    chore: adds lint-staged to prevent linting errors from being committed  (#1060)
    * chore: adds lint-staged.
    
    * chore: linting.

--- tests-node/unit/broccoli/translation-reducer/find-missing-translations-test.js
+++ tests-node/unit/broccoli/translation-reducer/find-missing-translations-test.js
@@ -9,21 +9,30 @@ const isTrue = () => true;
 describe('findMissingTranslations', function() {
   it('finds nothing if nothing is missing', function() {
     const key = 'foo';
-    const localeKeys = [['de', ['foo']], ['en', ['foo']]];
+    const localeKeys = [
+      ['de', ['foo']],
+      ['en', ['foo']]
+    ];
 
     expect(findMissingTranslations(key, localeKeys, isTrue)).to.deep.equal([]);
   });
 
   it('finds missing translations', function() {
     const key = 'foo';
-    const localeKeys = [['de', ['foo']], ['en', ['bar']]];
+    const localeKeys = [
+      ['de', ['foo']],
+      ['en', ['bar']]
+    ];
 
     expect(findMissingTranslations(key, localeKeys, isTrue)).to.deep.equal(['en']);
   });
 
   it('ignores missing translations if not required', function() {
     const key = 'foo';
-    const localeKeys = [['de', ['foo']], ['en', ['bar']]];
+    const localeKeys = [
+      ['de', ['foo']],
+      ['en', ['bar']]
+    ];
     const isAllowed = (key, locale) => locale !== 'en';
 
     expect(findMissingTranslations(key, localeKeys, isAllowed)).to.deep.equal([]);
