6c39c8c15750d345a85cef6766fac3707a51a0e3
Jason Mitchell <jason.mitchell.w@gmail.com>
2019-11-26 20:46:52 -0800

    fix: App translations should beat Addon translations if there is a collision (#1059)

--- tests-node/unit/broccoli/translation-reducer/wrap-with-namespace-if-needed.js
+++ tests-node/unit/broccoli/translation-reducer/wrap-with-namespace-if-needed.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
 'use strict';
 
-let expect = require('chai').expect;
+const expect = require('chai').expect;
 
-let wrapWithNamespaceIfNeeded = require('../../../../lib/broccoli/translation-reducer/utils/wrap-with-namespace-if-needed');
+const wrapWithNamespaceIfNeeded = require('../../../../lib/broccoli/translation-reducer/utils/wrap-with-namespace-if-needed');
+const enums = require('../../../../lib/broccoli/enums');
 
 describe('wrapWithNamespaceIfNeeded', function() {
   [
@@ -17,14 +18,14 @@ describe('wrapWithNamespaceIfNeeded', function() {
     [
       { a: true, b: false },
       { a: true, b: false },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/an-addon/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/an-addon/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['an-addon', 'other-addon']
     ],
     [
       { a: true, b: false },
       { a: true, b: false },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/@a-scoped/addon/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/@a-scoped/addon/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['@a-scoped/addon', 'an-addon', 'other-addon']
     ],
@@ -52,42 +53,42 @@ describe('wrapWithNamespaceIfNeeded', function() {
     [
       { a: true, b: false },
       { foo: { bar: { a: true, b: false } } },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/an-addon/foo/bar/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/an-addon/foo/bar/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['an-addon', 'other-addon']
     ],
     [
       { a: true, b: false },
       { foo: { bar: { a: true, b: false } } },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/an-addon/foo/bar/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/an-addon/foo/bar/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['other-addon', 'an-addon']
     ],
     [
       { a: true, b: false },
       { foo: { bar: { a: true, b: false } } },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/@a-scoped/addon/foo/bar/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/@a-scoped/addon/foo/bar/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['@a-scoped/addon', 'other-addon', 'an-addon']
     ],
     [
       { a: true, b: false },
       { foo_bar: { a: true, b: false } },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/@a-scoped/addon/foo_bar/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/@a-scoped/addon/foo_bar/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['@a-scoped/addon', 'other-addon', 'an-addon']
     ],
     [
       { a: true, b: false },
       { 'foo-bar': { a: true, b: false } },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/@a-scoped/addon/foo-bar/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/@a-scoped/addon/foo-bar/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['@a-scoped/addon', 'other-addon', 'an-addon']
     ],
     [
       { a: true, b: false },
       { foo_bar: { a: true, b: false } },
-      '/tmp/broccoli_debug-output_path-l4iBcmcT/@a-scoped/addon/foo bar/en-us.json',
+      `/tmp/broccoli_debug-output_path-l4iBcmcT/${enums.addonNamespace}/@a-scoped/addon/foo bar/en-us.json`,
       '/tmp/broccoli_debug-output_path-l4iBcmcT',
       ['@a-scoped/addon', 'other-addon', 'an-addon']
     ]
