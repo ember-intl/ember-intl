61f882102727aad7485ee0e0109352ea32c8f1c0
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-12-12 09:59:56 +0100

    fix(blueprints): Installation of ember-intl in addon use correct app,config paths

--- blueprints/ember-intl/index.js
+++ blueprints/ember-intl/index.js
@@ -4,11 +4,31 @@
  * Copyright 2015, Yahoo! Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
+let path = require('path');
 
 module.exports = {
   description: 'Setup ember-intl',
 
   normalizeEntityName() {},
+
+  fileMapTokens() {
+    return {
+      __app__(options) {
+        if (options.inAddon) {
+          return path.join('tests', 'dummy', 'app');
+        } else {
+          return path.join('app');
+        }
+      },
+      __config__(options) {
+        if (options.inAddon) {
+          return path.join('tests', 'dummy', 'config');
+        } else {
+          return path.join('config');
+        }
+      }
+    };
+  },
 
   afterInstall() {
     this.ui.writeLine(
