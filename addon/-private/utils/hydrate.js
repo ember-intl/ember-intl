bae89e8cd6534aa64c6bbf32749ff6a6cd5398c5
Jason Mitchell <jason.mitchell.w@gmail.com>
2020-04-04 22:39:56 -0700

    feat: translations written and read from fixed module location. (#1219)

--- addon/-private/utils/hydrate.js
+++ addon/-private/utils/hydrate.js
@@ -1,28 +1,11 @@
-/* globals requirejs */
+import translations from 'ember-intl/translations';
 
 /**
  * @private
  * @hide
  */
-export function lookupByFactoryType(type, modulePrefix) {
-  return Object.keys(requirejs.entries).filter((key) => {
-    return key.indexOf(`${modulePrefix}/${type}/`) === 0;
-  });
-}
-
-/**
- * Peeks into the requirejs map and registers all locale data (CLDR & Translations) found.
- *
- * @private
- * @hide
- */
-export default function (service, owner) {
-  const config = owner.resolveRegistration('config:environment');
-  const translations = lookupByFactoryType('translations', config.modulePrefix);
-
-  translations.forEach((moduleName) => {
-    const localeName = moduleName.split('/').pop();
-
-    service.addTranslations(localeName, owner.resolveRegistration(`translation:${localeName}`));
+export default function hydrate(service) {
+  translations.forEach(([locale, translations]) => {
+    service.addTranslations(locale, translations);
   });
 }
---
+++ addon/translations.js
@@ -0,0 +1,7 @@
+/**
+ * NOTE: this is a generated file that is produced at build time
+ * during `treeForAddon`.
+ * @private
+ * @hide
+ */
+export default [];
