97a769312e2198b4d316b20e9cae0677fb678b14
Alexander Sandström <mail+github@a16m.se>
2020-04-08 16:03:12 +0200

    Skip ICU warnings for non-required translation
    When translations aren't required, it makes sense to skip the ICU arguments warning too.

--- tests-node/unit/broccoli/translation-reducer/find-missing-icu-arguments-test.js
+++ tests-node/unit/broccoli/translation-reducer/find-missing-icu-arguments-test.js
@@ -2,6 +2,8 @@ const expect = require('chai').expect;
 
 const findMissingICUArguments = require('../../../../lib/broccoli/translation-reducer/linter/find-missing-icu-arguments');
 
+const isTrue = () => true;
+
 describe('findMissingICUArguments', function () {
   it('finds nothing if nothing is missing', function () {
     const key = 'foo';
@@ -12,7 +14,7 @@ describe('findMissingICUArguments', function () {
       en: { foo: ['aa'] },
     };
 
-    expect(findMissingICUArguments(key, allIcuArguments, locales, icuArguments)).to.deep.equal([]);
+    expect(findMissingICUArguments(key, allIcuArguments, locales, icuArguments, isTrue)).to.deep.equal([]);
   });
 
   it('finds missing icu arguments', function () {
@@ -24,9 +26,24 @@ describe('findMissingICUArguments', function () {
       en: { baz: ['arg1', 'arg2'] },
     };
 
-    expect(findMissingICUArguments(key, allIcuArguments, locales, icuArguments)).to.deep.equal([
+    expect(findMissingICUArguments(key, allIcuArguments, locales, icuArguments, isTrue)).to.deep.equal([
       ['de', ['arg1']],
       ['en', ['arg0']],
+    ]);
+  });
+
+  it('ignores missing icu arguments if not required', function() {
+    const key = 'baz';
+    const allIcuArguments = { baz: ['arg0', 'arg1', 'arg2'] };
+    const locales = ['de', 'en'];
+    const icuArguments = {
+      de: { baz: ['arg0', 'arg2'] },
+      en: { baz: ['arg1', 'arg2'] },
+    };
+    const isAllowed = (key, locale) => locale !== 'en';
+
+    expect(findMissingICUArguments(key, allIcuArguments, locales, icuArguments, isAllowed)).to.deep.equal([
+      ['de', ['arg1']]
     ]);
   });
 });
