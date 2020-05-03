2cca033db2649fe0d054f587c35ab474b1592d66
Jason Mitchell <jason.mitchell.w@gmail.com>
2020-05-02 19:45:43 -0700

    fix: polyfill docs with intl.relativetimeformat polyfill to support Safari 13.

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,6 +3,11 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import '@formatjs/intl-relativetimeformat/polyfill';
+import '@formatjs/intl-relativetimeformat/dist/locale-data/en';
+import '@formatjs/intl-relativetimeformat/dist/locale-data/fr';
+import '@formatjs/intl-relativetimeformat/dist/locale-data/es';
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
