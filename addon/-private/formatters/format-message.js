c61ed106f643a8653163fda2aa42bfdb40734b97
Alexander Sandström <mail+github@a16m.se>
2020-05-07 14:30:54 +0200

    Minor refactor, for readability
    It took me some time to read the previous code. I think something like this is quicker to grasp.
    
    Feel free to drop this commit before merging if you disagree.

--- addon/-private/formatters/format-message.js
+++ addon/-private/formatters/format-message.js
@@ -10,36 +10,37 @@ import IntlMessageFormat from 'intl-messageformat';
 import parse from '../utils/parse';
 import Formatter from './-base';
 
-const { keys } = Object;
-
 const {
   Handlebars: {
     Utils: { escapeExpression },
   },
 } = Ember;
 
-function escape(object) {
+function escapeOptions(object) {
   if (typeof object !== 'object') {
     return;
   }
 
-  return keys(object).reduce(
-    (accum, key) => {
-      if (isHTMLSafe(object[key])) {
-        // If the option is an instance of Ember SafeString,
-        // we don't want to pass it into the formatter, since the
-        // formatter won't know what to do with it. Instead, we cast
-        // the SafeString to a regular string using `toHTML`.
-        // Since it was already marked as safe we should *not* escape it.
-        accum[key] = object[key].toHTML();
-      } else if (typeof object[key] === 'string') {
-        accum[key] = escapeExpression(object[key]);
-      }
+  let escapedOpts = {};
 
-      return accum;
-    },
-    { ...object }
-  );
+  Object.keys(object).forEach((key) => {
+    let val = object[key];
+
+    if (isHTMLSafe(val)) {
+      // If the option is an instance of Ember SafeString,
+      // we don't want to pass it into the formatter, since the
+      // formatter won't know what to do with it. Instead, we cast
+      // the SafeString to a regular string using `toHTML`.
+      // Since it was already marked as safe we should *not* escape it.
+      escapedOpts[key] = val.toHTML();
+    } else if (typeof val === 'string') {
+      escapedOpts[key] = escapeExpression(val);
+    } else {
+      escapedOpts[key] = val; // copy as-is
+    }
+  });
+
+  return escapedOpts;
 }
 
 /**
@@ -68,7 +69,7 @@ export default class FormatMessage extends Formatter {
 
     const isHTMLSafe = options && options.htmlSafe;
     const formatterInstance = this.createNativeFormatter(ast, locale, this.readFormatConfig());
-    const escapedOptions = isHTMLSafe ? escape(options) : options;
+    const escapedOptions = isHTMLSafe ? escapeOptions(options) : options;
     const result = formatterInstance.format(escapedOptions);
 
     return isHTMLSafe ? htmlSafe(result) : result;
