c2077c5b5e23bb9a755f82b13fcc290e7d450cf6
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-06-20 16:44:28 -0400

    fix: Invalid import reference to default export of intl-messageformat-parser (#1363)

--- addon/-private/utils/parse.js
+++ addon/-private/utils/parse.js
@@ -1,11 +1,11 @@
-import IntlMessageFormat from 'intl-messageformat-parser';
+import { parse } from 'intl-messageformat-parser';
 
 /**
  * @private
  * @hide
  */
 export default function parseString(string) {
-  return IntlMessageFormat.parse(string, {
+  return parse(string, {
     normalizeHashtagInPlural: false,
   });
 }
