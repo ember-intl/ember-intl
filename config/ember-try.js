6aa94a71f9c7a6e578855bcd90e452da699d6704
Jan Buschtöns <jan@buschtoens.me>
2020-08-06 11:53:39 +0200

    chore(ci): add `ember-lts-2.20`

--- config/ember-try.js
+++ config/ember-try.js
@@ -69,6 +69,14 @@ module.exports = function () {
           },
         },
         {
+          name: 'ember-lts-3.20',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.20.0',
+            },
+          },
+        },
+        {
           name: 'ember-release',
           npm: {
             devDependencies: {
