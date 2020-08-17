5c314a708bc7a39f0c399a2fe3c04a9ea7002d61
Long Ho <holevietlong@gmail.com>
2020-08-17 14:13:52 -0400

    docs: remove references to unmaintained packages, update formatjs packages

--- tests/dummy/app/intl-polyfills.js
+++ tests/dummy/app/intl-polyfills.js
@@ -1,8 +1,8 @@
-import '@formatjs/intl-relativetimeformat/polyfill';
-import '@formatjs/intl-relativetimeformat/locale-data/en';
-import '@formatjs/intl-relativetimeformat/locale-data/fr';
-import '@formatjs/intl-relativetimeformat/locale-data/es';
 import '@formatjs/intl-pluralrules/polyfill';
 import '@formatjs/intl-pluralrules/locale-data/en';
 import '@formatjs/intl-pluralrules/locale-data/fr';
 import '@formatjs/intl-pluralrules/locale-data/es';
+import '@formatjs/intl-relativetimeformat/polyfill';
+import '@formatjs/intl-relativetimeformat/locale-data/en';
+import '@formatjs/intl-relativetimeformat/locale-data/fr';
+import '@formatjs/intl-relativetimeformat/locale-data/es';
