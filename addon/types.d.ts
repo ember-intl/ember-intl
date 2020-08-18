33b5f968c5f1fd96b222d98a7d51988c44bd9ccb
Jan Buschtöns <jan@buschtoens.me>
2020-08-18 17:09:40 +0200

    refactor: extend `Formats` from `intl-messageformat`

--- addon/types.d.ts
+++ addon/types.d.ts
@@ -1,8 +1,6 @@
+import type { Formats as BaseFormats } from 'intl-messageformat';
 import { RelativeTimeFormatOptions } from './-private/formatters/format-relative';
 
-export interface Formats {
-  date: Record<string, Intl.DateTimeFormatOptions>;
-  time: Record<string, Intl.DateTimeFormatOptions>;
+export interface Formats extends BaseFormats {
   relative: Record<string, RelativeTimeFormatOptions>;
-  number: Record<string, Intl.NumberFormatOptions>;
 }
