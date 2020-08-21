20c42f5669faf90107cff6c5d58a44ffda9a4439
Jan Buschtöns <jan@buschtoens.me>
2020-08-21 15:11:41 +0200

    chore(tests): migrate test-helper

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,8 +1,9 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import 'ember-cli-testdouble-qunit';
+import 'qunit-dom';
 
 setApplication(Application.create(config.APP));
 
