2fd3ed263463ba32ec179634c842d05f86af1258
Jan Buschtöns <jan@buschtoens.me>
2020-08-21 15:17:08 +0200

    feat: re-export test-support `TestContext`

--- addon-test-support/index.ts
+++ addon-test-support/index.ts
@@ -2,3 +2,4 @@ export { default as t } from './t';
 export { default as setLocale } from './set-locale';
 export { default as addTranslations } from './add-translations';
 export { default as setupIntl } from './setup-intl';
+export type { TestContext, IntlTestContext } from './setup-intl';
