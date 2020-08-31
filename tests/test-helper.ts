782e7f46545bf0764e5de107aa11159c7a2e5781
Jan Buschtöns <jan@buschtoens.me>
2020-08-31 13:49:07 +0200

    refactor(deps): use `testdouble-qunit` directly
    Brings along better type support and removes an unnecessary addon.

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -2,8 +2,14 @@ import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import 'ember-cli-testdouble-qunit';
 import 'qunit-dom';
+import QUnit from 'qunit';
+import td from 'testdouble';
+import installVerifyAssertion from 'testdouble-qunit';
+
+// @ts-expect-error The types are incorrect.
+// https://github.com/alexlafroscia/testdouble-qunit/tree/master/packages/testdouble-qunit
+installVerifyAssertion(QUnit, td);
 
 setApplication(Application.create(config.APP));
 
