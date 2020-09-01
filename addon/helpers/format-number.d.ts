2acbb9970152cf3e183bc791f7c260f477af251a
Jan Buschtöns <jan@buschtoens.me>
2020-09-01 11:23:57 +0200

    feat(helpers): add type defintions
    When runnign `ember ts:precompile` different options are used and types for helpers are required as well.

---
+++ addon/helpers/format-number.d.ts
@@ -0,0 +1,10 @@
+import BaseHelper from '../-private/helpers/-format-base';
+import IntlService from '../services/intl';
+
+type Params = Parameters<IntlService['formatNumber']>;
+
+export default class FormatNumberHelper extends BaseHelper<Params[0], Params[1]> {
+  allowEmpty: false;
+
+  format(value: Params[0], options?: Params[1]): string;
+}
