8517ac84c71725cbfe9ddadf2f969bfb8450a5d8
Jan Buschtöns <jan@buschtoens.me>
2020-09-08 20:36:52 +0200

    fix(extract-icu-arguments): legacy HTML format

--- addon/-private/utils/parse.ts
+++ addon/-private/utils/parse.ts
@@ -5,7 +5,7 @@ import { parse } from 'intl-messageformat-parser';
  * @hide
  */
 export default function parseString(string: string) {
-  // ! Sync with `lib/message-validator/validate-message.js`
+  // ! Sync with `lib/parse-options.js`
   return parse(string, {
     normalizeHashtagInPlural: false,
     ignoreTag: true,
