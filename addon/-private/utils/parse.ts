d3bbacc721829c50581af0bc94a2cec10ceee555
Jan Buschtöns <jan@buschtoens.me>
2020-09-08 20:33:18 +0200

    fix(message-validator): accept legacy HTML format
    Fixes #1443. 🎉

--- addon/-private/utils/parse.ts
+++ addon/-private/utils/parse.ts
@@ -5,6 +5,7 @@ import { parse } from 'intl-messageformat-parser';
  * @hide
  */
 export default function parseString(string: string) {
+  // ! Sync with `lib/message-validator/validate-message.js`
   return parse(string, {
     normalizeHashtagInPlural: false,
     ignoreTag: true,
