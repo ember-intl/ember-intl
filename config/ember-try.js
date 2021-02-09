6f51647aa147654a6a3c1e5a61cb42cbf73dde07
Long Ho <holevietlong@gmail.com>
2021-02-08 23:28:42 -0500

    chore: rm unsupported ember LTS versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,59 +8,6 @@ module.exports = function () {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-cli-shims': '^1.2.0',
-              'ember-data': '~3.12.0',
-              'ember-source': '~2.12.0',
-              '@ember/jquery': '^0.5.2',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0',
-              '@ember/jquery': '^0.5.2',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0',
-              '@ember/jquery': '^0.5.2',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.4',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.4.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.8',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.8.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.12.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.16',
           npm: {
             devDependencies: {
