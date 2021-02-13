504f09089ae9481d2182e792fcbc4dc57edf6e0f
Long Ho <holevietlong@gmail.com>
2021-02-12 20:19:23 -0500

    chore: deps++

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -7,7 +7,6 @@ import QUnit from 'qunit';
 import td from 'testdouble';
 import installVerifyAssertion from 'testdouble-qunit';
 
-// @ts-expect-error The types are incorrect.
 // https://github.com/alexlafroscia/testdouble-qunit/tree/master/packages/testdouble-qunit
 installVerifyAssertion(QUnit, td);
 
