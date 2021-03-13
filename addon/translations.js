234eeb94b4d33095a68b9762dd42e2c104a426fb
Jason Mitchell <jason.mitchell.w@gmail.com>
2021-03-13 11:24:05 -0800

    chore: cleanup addon/translations.js comment

--- addon/translations.js
+++ addon/translations.js
@@ -2,8 +2,6 @@
  * NOTE: this is a generated file that is produced at build time during
  * `treeForAddon`, if `publicOnly` is disabled.
  *
- * If `publicOnly` is enabled, we'll still want this empty export to be present,
- * to that the import in `-private/utils/hydrate.ts` works unconditionally.
  * @private
  * @hide
  */
