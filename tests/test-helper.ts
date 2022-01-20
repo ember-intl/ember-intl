0d3c3bde0df9c3e124f066e73f0c5de02877ba2d
Jan Buschtöns <jan@buschtoens.me>
2022-01-20 01:15:42 +0100

    chore(deps): update `qunit` setup
    Fixes the following runtime error caused by an outdated transitive `ember-test-helpers` dependency:
    
    > Could not find module `@ember/application/deprecations` imported from `@ember/string/index`
    
    https://github.com/emberjs/ember.js/issues/19668
    
    https://github.com/emberjs/ember-qunit/blob/master/docs/migration.md

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -2,13 +2,15 @@ import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import 'qunit-dom';
+import { setup } from 'qunit-dom';
 import QUnit from 'qunit';
 import td from 'testdouble';
 import installVerifyAssertion from 'testdouble-qunit';
 
 // https://github.com/alexlafroscia/testdouble-qunit/tree/master/packages/testdouble-qunit
 installVerifyAssertion(QUnit, td);
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
