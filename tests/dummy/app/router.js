488e909cd27c56a5f7fb062882e01ab4e823bd4b
ijlee2 <ijlee2@users.noreply.github.com>
2022-06-10 19:20:59 +0200

    chore: Recreated the docs.guide.migration-4-0-to-5-0 route

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -16,6 +16,7 @@ Router.map(function () {
     this.route('guide', function () {
       this.route('addon-configs');
       this.route('asynchronously-loading-translations');
+      this.route('migration-4-0-to-5-0');
     });
   });
 });
