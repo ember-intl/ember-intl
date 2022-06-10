6446d3cae4a68e99b31a6ba84a7e6680e9512f3a
ijlee2 <ijlee2@users.noreply.github.com>
2022-06-10 19:22:12 +0200

    chore: Recreated the docs.guide.missing-translations route

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -17,6 +17,7 @@ Router.map(function () {
       this.route('addon-configs');
       this.route('asynchronously-loading-translations');
       this.route('migration-4-0-to-5-0');
+      this.route('missing-translations');
     });
   });
 });
