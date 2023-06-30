f0edd92a5b9b33787ef3815933058ad6e8f92f7e
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-30 23:50:29 +0200

    chore: Checked compatibilities with Ember 4.4, 4.8, and 4.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -24,6 +24,30 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
