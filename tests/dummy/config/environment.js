7947f937e0d4cec4dd69121a4d509909addcbd07
ijlee2 <ijlee2@users.noreply.github.com>
2023-07-01 15:06:40 +0200

    chore: Ran ember-cli-update

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = function (environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'dummy',
     environment,
     rootURL: '/',
     locationType: 'history',
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
-      EXTEND_PROTOTYPES: false,
     },
 
     APP: {
