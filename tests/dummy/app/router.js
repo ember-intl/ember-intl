1b8ec86952f95019335b6a55b95d5e2f98d309fc
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-09-19 17:26:47 +0200

    Documented known breaking changes for v6.1.0 (#1784)
    * chore: Moved the migration guide for v4 to legacy
    
    * chore: Scaffolded the migration guide from v5 to v6.1
    
    * chore: Documented how to migrate from v5.7.2 to v6.1.0
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -22,9 +22,9 @@ Router.map(function () {
     });
 
     this.route('guide', function () {
+      this.route('migration-5-0-to-6-1');
       this.route('addon-configs');
       this.route('asynchronously-loading-translations');
-      this.route('migration-4-0-to-5-0');
       this.route('missing-translations');
       this.route('service-api');
       this.route('testing');
@@ -49,6 +49,7 @@ Router.map(function () {
     this.route('legacy', function () {
       this.route('migration-2-0-to-3-0');
       this.route('migration-3-0-to-4-0');
+      this.route('migration-4-0-to-5-0');
       this.route('v2');
     });
   });
