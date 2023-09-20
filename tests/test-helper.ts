dfe530d63dc24f012f76c21d0ef9d06023652452
ijlee2 <ijlee2@users.noreply.github.com>
2023-09-20 13:23:18 +0200

    chore: Removed testdouble (unnecessary)

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -4,11 +4,6 @@ import config from 'dummy/config/environment';
 import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
-import td from 'testdouble';
-import installVerifyAssertion from 'testdouble-qunit';
-
-// https://github.com/alexlafroscia/testdouble-qunit/tree/master/packages/testdouble-qunit
-installVerifyAssertion(QUnit, td);
 
 setApplication(Application.create(config.APP));
 
