49353dc03f2355073a4f4270a22bf94255fbcbe6
ijlee2 <ijlee2@users.noreply.github.com>
2023-11-09 11:28:17 +0100

    refactor: Extended the shared configurations (test-node)

--- test-node/.prettierrc.cjs
+++ test-node/.prettierrc.cjs
@@ -1,20 +1,3 @@
 'use strict';
 
-module.exports = {
-  overrides: [
-    {
-      files: '*.hbs',
-      options: {
-        printWidth: 64,
-        singleQuote: false,
-      },
-    },
-    {
-      files: '*.{cjs,cts,gjs,gts,js,mjs,mts,ts}',
-      options: {
-        printWidth: 80,
-        singleQuote: true,
-      },
-    },
-  ],
-};
+module.exports = require('@shared-configs/prettier');
