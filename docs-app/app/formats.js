f7e45f887f8a70cf5417862da82b2db9dfbb0637
ijlee2 <ijlee2@users.noreply.github.com>
2023-11-10 11:37:53 +0100

    chore: Configured ember-intl

---
+++ docs-app/app/formats.js
@@ -0,0 +1,43 @@
+export default {
+  date: {
+    hhmmss: {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    },
+  },
+
+  number: {
+    EUR: {
+      currency: 'EUR',
+      style: 'currency',
+    },
+
+    JPY: {
+      currency: 'JPY',
+      style: 'currency',
+    },
+
+    USD: {
+      currency: 'USD',
+      style: 'currency',
+    },
+
+    compact: {
+      notation: 'compact',
+    },
+
+    currency: {
+      minimumFractionDigits: 2,
+      style: 'currency',
+    },
+  },
+
+  time: {
+    hhmmss: {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    },
+  },
+};
