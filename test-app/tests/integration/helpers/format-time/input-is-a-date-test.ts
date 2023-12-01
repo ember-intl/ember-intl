f0168933e0e5e8c17a7c80269d85734bbfd17983
ijlee2 <ijlee2@users.noreply.github.com>
2023-12-01 16:04:18 +0100

    refactor: Added tests for {{format-time}} helper

---
+++ test-app/tests/integration/helpers/format-time/input-is-a-date-test.ts
@@ -0,0 +1,82 @@
+import {
+  render,
+  type TestContext as BaseTestContext,
+} from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+import { module, test } from 'qunit';
+import { setupRenderingTest, updateLocale } from 'test-app/tests/helpers';
+
+interface TestContext extends BaseTestContext {
+  date: Date;
+}
+
+module(
+  'Integration | Helper | format-time > input is a Date',
+  function (hooks) {
+    setupRenderingTest(hooks);
+    setupIntl(hooks, 'en-us');
+
+    hooks.beforeEach(function (this: TestContext) {
+      this.date = new Date('2014-01-23T18:00:44');
+    });
+
+    test('it returns a string', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output>
+          {{format-time this.date}}
+        </div>
+      `);
+
+      assert.dom('[data-test-output]').hasText('6:00 PM');
+    });
+
+    test('it returns a new value when the locale is changed', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output>
+          {{format-time this.date}}
+        </div>
+      `);
+
+      await updateLocale('de-de');
+
+      assert.dom('[data-test-output]').hasText('18:00');
+    });
+
+    test('we can format the date', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output="1">
+          {{format-time this.date format='hhmmss'}}
+        </div>
+
+        <div data-test-output="2">
+          {{format-time
+            this.date
+            hour="2-digit"
+            minute="numeric"
+          }}
+        </div>
+      `);
+
+      assert.dom('[data-test-output="1"]').hasText('6:00:44 PM');
+
+      assert.dom('[data-test-output="2"]').hasText('06:00 PM');
+    });
+
+    test('we can specify the time zone', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output>
+          {{format-time this.date timeZone='UTC'}}
+        </div>
+      `);
+
+      assert.dom('[data-test-output]').hasText(
+        new Intl.DateTimeFormat('en-us', {
+          hour: 'numeric',
+          minute: 'numeric',
+          timeZone: 'UTC',
+        }).format(new Date('2014-01-23T18:00:44')),
+      );
+    });
+  },
+);
---
+++ test-app/tests/integration/helpers/format-time/input-is-an-integer-test.ts
@@ -0,0 +1,83 @@
+import {
+  render,
+  type TestContext as BaseTestContext,
+} from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+import { module, test } from 'qunit';
+import { setupRenderingTest, updateLocale } from 'test-app/tests/helpers';
+
+interface TestContext extends BaseTestContext {
+  date: number;
+}
+
+module(
+  'Integration | Helper | format-time > input is an integer',
+  function (hooks) {
+    setupRenderingTest(hooks);
+    setupIntl(hooks, 'en-us');
+
+    hooks.beforeEach(function (this: TestContext) {
+      this.date = new Date('2014-01-23T18:00:44').getTime();
+    });
+
+    test('it returns a string', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output>
+          {{format-time this.date}}
+        </div>
+      `);
+
+      assert.dom('[data-test-output]').hasText('6:00 PM');
+    });
+
+    test('it returns a new value when the locale is changed', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output>
+          {{format-time this.date}}
+        </div>
+      `);
+
+      await updateLocale('de-de');
+
+      assert.dom('[data-test-output]').hasText('18:00');
+    });
+
+    test('we can format the date', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output="1">
+          {{format-time this.date format='hhmmss'}}
+        </div>
+
+        <div data-test-output="2">
+          {{format-time
+            this.date
+            hour="2-digit"
+            minute="numeric"
+            month="long"
+          }}
+        </div>
+      `);
+
+      assert.dom('[data-test-output="1"]').hasText('6:00:44 PM');
+
+      assert.dom('[data-test-output="2"]').hasText('January at 06:00 PM');
+    });
+
+    test('we can specify the time zone', async function (this: TestContext, assert) {
+      await render<TestContext>(hbs`
+        <div data-test-output>
+          {{format-time this.date timeZone='UTC'}}
+        </div>
+      `);
+
+      assert.dom('[data-test-output]').hasText(
+        new Intl.DateTimeFormat('en-us', {
+          hour: 'numeric',
+          minute: 'numeric',
+          timeZone: 'UTC',
+        }).format(new Date('2014-01-23T18:00:44')),
+      );
+    });
+  },
+);
