c2f92e78c35a98464d97d1fca7166b86f665d595
ijlee2 <ijlee2@users.noreply.github.com>
2023-12-11 22:05:41 +0100

    chore: Simplified unit tests for intl()

--- test-app/tests/unit/macros/intl-test.ts
+++ test-app/tests/unit/macros/intl-test.ts
@@ -1,136 +1,151 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
 import { setOwner } from '@ember/application';
-import EmberObject, { getProperties, set } from '@ember/object';
+import EmberObject, { set } from '@ember/object';
 import { run } from '@ember/runloop';
-import type { TestContext as BaseTestContext } from '@ember/test-helpers';
 import type { IntlService } from 'ember-intl';
 import { intl } from 'ember-intl/macros';
-import { __intlInjectionName } from 'ember-intl/macros/intl';
-import type {} from 'ember-intl/test-support';
-import { setupIntl } from 'ember-intl/test-support';
+import { setLocale, setupIntl } from 'ember-intl/test-support';
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
-interface TestContext extends BaseTestContext {
-  ContainerObject: typeof EmberObject;
-  intl: IntlService;
-}
+class ContainerObject extends EmberObject {}
 
-module('Unit | Macros | intl', function (hooks) {
+module('Unit | Macro | intl', function (hooks) {
   setupTest(hooks);
   setupIntl(hooks);
 
-  hooks.beforeEach(function (this: TestContext) {
-    const { owner } = this;
+  test('looks up the intl service through the owner and injects the service invisibly', function (assert) {
+    const intlService = this.owner.lookup('service:intl');
 
-    // eslint-disable-next-line ember/no-classic-classes
-    this.ContainerObject = EmberObject.extend({
-      init() {
-        this._super();
-        setOwner(this, owner);
-      },
-    });
-  });
+    const emberObject = ContainerObject.extend({
+      someProperty: intl(function (_intl: IntlService) {
+        assert.strictEqual(_intl, intlService);
 
-  test('looks up the intl service through the owner and injects the service invisibly', function (this: TestContext, assert) {
-    const object = this.ContainerObject.extend({
-      property: intl((intl) =>
-        assert.strictEqual(
-          intl,
-          this.intl,
-          'service is accessible in CP callback',
-        ),
-      ),
-    }).create();
-
-    object.property;
-
-    assert.ok(
-      Object.getOwnPropertyNames(object).includes(__intlInjectionName),
-      'service is injected',
-    );
-    assert.notOk(
-      Object.keys(object).includes(__intlInjectionName),
-      'service is non-enumerable',
-    );
-  });
-
-  test('does not use or clobber the pre-existing intl injection', function (this: TestContext, assert) {
-    const IDENTITY = {};
-
-    const object = this.ContainerObject.extend({
-      intl: IDENTITY,
-      property: intl((intl) => assert.strictEqual(intl, this.intl)),
-    }).create();
-
-    object.property;
-
-    assert.strictEqual(object.intl, IDENTITY);
-  });
-
-  test('passes the propertyKey, context, and binds to it', function (this: TestContext, assert) {
-    const object = this.ContainerObject.extend({
-      property: intl(function (_intl, propertyKey, ctx) {
-        assert.strictEqual(propertyKey, 'property', 'passes propertyKey');
-        assert.strictEqual(ctx, object, 'passes context');
-        assert.strictEqual(this, object, 'binds to the instance');
+        assert.step('Called intl()');
       }),
     }).create();
 
-    object.property;
+    setOwner(emberObject, this.owner);
+
+    emberObject.get('someProperty');
+
+    const propertyNames = Object.getOwnPropertyNames(emberObject);
+    const keys = Object.keys(emberObject);
+
+    assert.true(
+      propertyNames.some((name) => name.includes('intl-')),
+      'service is injected',
+    );
+
+    assert.false(
+      keys.some((name) => name.includes('intl-')),
+      'service is non-enumerable',
+    );
+
+    assert.verifySteps(['Called intl()']);
   });
 
-  test('uses the return value of the passed function as the computed property value', function (this: TestContext, assert) {
+  test('does not use or clobber the pre-existing intl injection', function (assert) {
+    const intlService = this.owner.lookup('service:intl');
+
     const IDENTITY = {};
 
-    const object = this.ContainerObject.extend({
-      property: intl(() => IDENTITY),
+    const emberObject = ContainerObject.extend({
+      intl: IDENTITY,
+
+      someProperty: intl(function (_intl: IntlService) {
+        assert.strictEqual(_intl, intlService);
+
+        assert.step('Called intl()');
+      }),
     }).create();
 
-    assert.strictEqual(object.property, IDENTITY);
+    setOwner(emberObject, this.owner);
+
+    emberObject.get('someProperty');
+
+    assert.strictEqual(emberObject.get('intl'), IDENTITY);
+
+    assert.verifySteps(['Called intl()']);
   });
 
-  test('listens for locale changes', function (this: TestContext, assert) {
-    this.intl.setLocale(['en-us']);
+  test('passes the propertyKey, context, and binds to it', function (assert) {
+    const emberObject = ContainerObject.extend({
+      someProperty: intl(function (
+        _intl: IntlService,
+        propertyKey: string,
+        ctx: any,
+      ) {
+        assert.strictEqual(propertyKey, 'someProperty');
 
-    const object = this.ContainerObject.extend({
-      property: intl((intl) => intl.locale),
+        assert.strictEqual(ctx, emberObject, 'passes context');
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore: 'this' implicitly has type 'any' because it does not have a type annotation.
+        assert.strictEqual(this, emberObject, 'binds to the instance');
+
+        assert.step('Called intl()');
+      }),
     }).create();
 
-    assert.deepEqual(object.property, ['en-us']);
+    setOwner(emberObject, this.owner);
 
-    run(() => this.intl.setLocale(['de-de']));
-    assert.deepEqual(object.property, ['de-de']);
+    emberObject.get('someProperty');
+
+    assert.verifySteps(['Called intl()']);
   });
 
-  test('accpets further dependent keys', function (this: TestContext, assert) {
-    const dependencies = { dependencyA: 1, dependencyB: 2, dependencyC: 3 };
-    const dependencyKeys = Object.keys(
-      dependencies,
-    ) as (keyof typeof dependencies)[];
+  test('uses the return value of the passed function as the computed property value', function (assert) {
+    const emberObject = ContainerObject.extend({
+      someProperty: intl(function () {
+        return 'some-value';
+      }),
+    }).create();
 
-    const object = this.ContainerObject.extend({
-      ...dependencies,
-      // ! TypeScript can't deal with spreading here, so we specify the keys explicitly
-      property: intl(
-        'dependencyA',
-        'dependencyB',
-        'dependencyC',
-        (_intl, _propertyKey, ctx) =>
-          getProperties(ctx as any, ...dependencyKeys),
+    setOwner(emberObject, this.owner);
+
+    assert.strictEqual(emberObject.get('someProperty'), 'some-value');
+  });
+
+  test('listens for locale changes', async function (assert) {
+    const emberObject = ContainerObject.extend({
+      someProperty: intl((_intl: IntlService) => {
+        return _intl.locale;
+      }),
+    }).create();
+
+    setOwner(emberObject, this.owner);
+
+    assert.deepEqual(emberObject.get('someProperty'), ['en-us']);
+
+    await setLocale('de-de');
+
+    assert.deepEqual(emberObject.get('someProperty'), ['de-de']);
+  });
+
+  test('accpets further dependent keys', function (assert) {
+    const emberObject = ContainerObject.extend({
+      key1: 1,
+      key2: 2,
+
+      someProperty: intl(
+        'key1',
+        'key2',
+        function (_intl: IntlService, _propertyKey: string, ctx: any) {
+          return [ctx.key1, ctx.key2].join(', ');
+        },
       ),
     }).create();
 
-    assert.deepEqual(object.property, dependencies);
+    setOwner(emberObject, this.owner);
 
-    run(() => set(object, 'dependencyA', 4));
-    assert.deepEqual(object.property, getProperties(object, ...dependencyKeys));
+    assert.strictEqual(emberObject.get('someProperty'), '1, 2');
 
-    run(() => set(object, 'dependencyB', 5));
-    assert.deepEqual(object.property, getProperties(object, ...dependencyKeys));
+    emberObject.set('key1', 3);
 
-    run(() => set(object, 'dependencyC', 6));
-    assert.deepEqual(object.property, getProperties(object, ...dependencyKeys));
+    assert.strictEqual(emberObject.get('someProperty'), '3, 2');
+
+    run(() => set(emberObject, 'key2', 4));
+
+    assert.strictEqual(emberObject.get('someProperty'), '3, 4');
   });
 });
