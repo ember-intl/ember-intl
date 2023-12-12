89f41a4569411cd5478b7c1520761a063ef032ba
ijlee2 <ijlee2@users.noreply.github.com>
2023-12-12 08:36:43 +0100

    chore: Removed existing types and reformatted code (intl)

--- ember-intl/addon/macros/intl.ts
+++ ember-intl/addon/macros/intl.ts
@@ -4,7 +4,7 @@ import type ComputedProperty from '@ember/object/computed';
 
 import type IntlService from '../services/intl';
 
-export type GetterFn<T, C = unknown> = (
+type GetterFn<T, C = unknown> = (
   this: C,
   intl: IntlService,
   key: string,
@@ -15,113 +15,13 @@ export type GetterFn<T, C = unknown> = (
  * @private
  * @hide
  */
-const __intlInjectionName = `intl-${Date.now().toString(
-  36,
-)}` as 'intl-PRIVATE-SYMBOL';
+const __intlInjectionName = `intl-${Date.now().toString(36)}`;
 
-export default function intl<T>(getterFn: GetterFn<T>): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  key4: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  key4: string,
-  key5: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  key4: string,
-  key5: string,
-  key6: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  key4: string,
-  key5: string,
-  key6: string,
-  key7: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  key4: string,
-  key5: string,
-  key6: string,
-  key7: string,
-  key8: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  key4: string,
-  key5: string,
-  key6: string,
-  key7: string,
-  key8: string,
-  key9: string,
-  getterFn: GetterFn<T>,
-): ComputedProperty<T>;
-export default function intl<T>(
-  key0: string,
-  key1: string,
-  key2: string,
-  key3: string,
-  key4: string,
-  key5: string,
-  key6: string,
-  key7: string,
-  key8: string,
-  key9: string,
-  ...rest: [string | GetterFn<T>][] // Better than nothing ¯\_(ツ)_/¯
-): ComputedProperty<T>;
 export default function intl<T>(
   ...dependentKeysAndGetterFn: unknown[]
 ): ComputedProperty<T> {
   const getterFn = dependentKeysAndGetterFn.pop() as GetterFn<T>;
+
   const dependentKeys = dependentKeysAndGetterFn as string[];
 
   return computed(`${__intlInjectionName}.locale`, ...dependentKeys, function (
@@ -135,7 +35,9 @@ export default function intl<T>(
         enumerable: false,
       });
     }
+
     const intl = this[__intlInjectionName]!;
+
     return getterFn.call(this, intl, propertyKey, this);
   } as any) as ComputedProperty<T>;
 }
