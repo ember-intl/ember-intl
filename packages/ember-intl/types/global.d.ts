59416beeb2a23c3836533cb314b990247635c664
ijlee2 <ijlee2@users.noreply.github.com>
2024-04-12 15:49:14 +0200

    chore: Removed @types/htmlbars-inline-precompile from devDependencies

--- packages/ember-intl/types/global.d.ts
+++ packages/ember-intl/types/global.d.ts
@@ -1,6 +1,6 @@
 // Types for compiled templates
 declare module 'ember-intl/templates/*' {
-  import type { TemplateFactory } from 'htmlbars-inline-precompile';
+  import type { TemplateFactory } from 'ember-cli-htmlbars';
   const tmpl: TemplateFactory;
   export default tmpl;
 }
