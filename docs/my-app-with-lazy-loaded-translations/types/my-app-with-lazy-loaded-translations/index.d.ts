0daacd3e5fc133d2bd15a0c9aabc9a09826f17b1
ijlee2 <ijlee2@users.noreply.github.com>
2024-05-10 13:48:36 +0200

    refactor: Removed duplicate code

--- docs/my-app-with-lazy-loaded-translations/types/my-app-with-lazy-loaded-translations/index.d.ts
+++ docs/my-app-with-lazy-loaded-translations/types/my-app-with-lazy-loaded-translations/index.d.ts
@@ -4,7 +4,6 @@ import 'ember-source/types';
 import 'ember-source/types/preview';
 
 import type EmberPageTitleRegistry from 'ember-page-title/template-registry';
-import type EmberTruthHelpersRegistry from 'ember-truth-helpers/template-registry';
 import type MyV2AddonRegistry from 'my-v2-addon/template-registry';
 
 import type EmberIntlRegistry from './ember-intl';
@@ -14,7 +13,6 @@ declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry
     extends EmberIntlRegistry,
       EmberPageTitleRegistry,
-      EmberTruthHelpersRegistry,
       MyV1AddonRegistry,
       MyV2AddonRegistry {
     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
