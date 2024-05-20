28a414c16ea553caea9c52d0d7fdae865efd3ad1
ijlee2 <ijlee2@users.noreply.github.com>
2024-05-20 06:39:22 +0200

    chore: Reintroduced setLocale()

--- packages/ember-intl/addon/services/intl.ts
+++ packages/ember-intl/addon/services/intl.ts
@@ -1,10 +1,18 @@
 import { getOwner } from '@ember/application';
+import { registerDestructor } from '@ember/destroyable';
+import { cancel, next, type Timer as EmberRunTimer } from '@ember/runloop';
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import EventEmitter from 'eventemitter3';
 
 import type { IntlShape, OnErrorFn } from '../-private/formatjs/index';
 import { createIntl, createIntlCache } from '../-private/formatjs/index';
-import { convertToString } from '../-private/utils/locale';
+import { getDOM } from '../-private/utils/get-dom';
+import {
+  convertToArray,
+  convertToString,
+  hasLocaleChanged,
+} from '../-private/utils/locale';
 
 type OnFormatjsError = (error: Parameters<OnErrorFn>[0]) => void;
 
@@ -13,7 +21,9 @@ export default class IntlService extends Service {
   @tracked private _locale?: string[];
 
   private _cache = createIntlCache();
+  private _eventEmitter = new EventEmitter();
   private _formats?: Record<string, unknown>;
+  private _timer?: EmberRunTimer;
 
   get locales(): string[] {
     return Object.keys(this._intls);
@@ -39,6 +49,35 @@ export default class IntlService extends Service {
 
   willDestroy() {
     super.willDestroy();
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore: Argument of type 'Timer | undefined' is not assignable to parameter of type 'EmberRunTimer | undefined'
+    // eslint-disable-next-line ember/no-runloop
+    cancel(this._timer);
+  }
+
+  setLocale(locale: string | string[]): void {
+    const proposedLocale = convertToArray(locale);
+
+    if (hasLocaleChanged(proposedLocale, this._locale)) {
+      this._locale = proposedLocale;
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore: Argument of type 'Timer | undefined' is not assignable to parameter of type 'EmberRunTimer | undefined'
+      // eslint-disable-next-line ember/no-runloop
+      cancel(this._timer);
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore: Argument of type 'Timer | undefined' is not assignable to parameter of type 'EmberRunTimer | undefined'
+      // eslint-disable-next-line ember/no-runloop
+      this._timer = next(() => {
+        this._eventEmitter.emit('localeChanged');
+
+        this.updateDocumentLanguage();
+      });
+    }
+
+    this.updateIntl(locale);
   }
 
   private createIntl(
@@ -68,6 +107,37 @@ export default class IntlService extends Service {
     return this._intls[resolvedLocale];
   }
 
+  private onFormatjsError(error: Parameters<OnFormatjsError>[0]): void {
+    switch (error.code) {
+      case 'MISSING_TRANSLATION': {
+        // Do nothing
+        break;
+      }
+
+      default: {
+        throw error;
+      }
+    }
+  }
+
+  private onLocaleChanged(fn: any, context: any) {
+    this._eventEmitter.on('localeChanged', fn, context);
+
+    registerDestructor(context, () => {
+      this._eventEmitter.off('localeChanged', fn, context);
+    });
+  }
+
+  private updateDocumentLanguage(): void {
+    const dom = getDOM(this);
+
+    if (dom) {
+      const html = dom.documentElement;
+
+      html.setAttribute('lang', this.primaryLocale);
+    }
+  }
+
   private updateIntl(
     locale: string | string[],
     messages?: Record<string, unknown>,
@@ -94,19 +164,6 @@ export default class IntlService extends Service {
       ...this._intls,
       [resolvedLocale]: newIntl,
     };
-  }
-
-  private onFormatjsError(error: Parameters<OnFormatjsError>[0]): void {
-    switch (error.code) {
-      case 'MISSING_TRANSLATION': {
-        // Do nothing
-        break;
-      }
-
-      default: {
-        throw error;
-      }
-    }
   }
 }
 
