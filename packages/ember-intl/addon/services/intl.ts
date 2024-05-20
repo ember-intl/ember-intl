1a6f750f7e47b0e742e891fd495d047c1232ae57
ijlee2 <ijlee2@users.noreply.github.com>
2024-05-20 06:38:40 +0200

    chore: Reintroduced internal state variables

--- packages/ember-intl/addon/services/intl.ts
+++ packages/ember-intl/addon/services/intl.ts
@@ -1,6 +1,39 @@
+import { getOwner } from '@ember/application';
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default class IntlService extends Service {}
+export default class IntlService extends Service {
+  @tracked private _intls: Record<string, unknown> = {};
+  @tracked private _locale?: string[];
+
+  private _formats?: Record<string, unknown>;
+
+  get locales(): string[] {
+    return Object.keys(this._intls);
+  }
+
+  get primaryLocale(): string | undefined {
+    if (!this._locale) {
+      return;
+    }
+
+    return this._locale[0];
+  }
+
+  constructor() {
+    // eslint-disable-next-line prefer-rest-params
+    super(...arguments);
+
+    if (!this._formats) {
+      // @ts-expect-error: Property 'resolveRegistration' does not exist on type 'Owner'
+      this._formats = getOwner(this).resolveRegistration('formats:main') ?? {};
+    }
+  }
+
+  willDestroy() {
+    super.willDestroy();
+  }
+}
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your services.
 declare module '@ember/service' {
