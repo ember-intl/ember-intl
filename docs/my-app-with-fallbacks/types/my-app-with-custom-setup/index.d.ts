1562c08baf3d7c3abe05c53ed23c190296e899c2
ijlee2 <ijlee2@users.noreply.github.com>
2024-06-10 07:23:19 +0200

    chore: Scaffolded my-app-with-fallbacks

---
+++ docs/my-app-with-fallbacks/types/my-app-with-custom-setup/index.d.ts
@@ -0,0 +1,21 @@
+import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
+import 'ember-source/types';
+import 'ember-source/types/preview';
+
+import type EmberIntlRegistry from 'ember-intl/template-registry';
+import type EmberPageTitleRegistry from 'ember-page-title/template-registry';
+import type MyV2AddonRegistry from 'my-v2-addon/template-registry';
+
+import type MyV1AddonRegistry from './my-v1-addon';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry
+    extends EmberIntlRegistry,
+      EmberPageTitleRegistry,
+      MyV1AddonRegistry,
+      MyV2AddonRegistry {
+    // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
+    // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
+  }
+}
