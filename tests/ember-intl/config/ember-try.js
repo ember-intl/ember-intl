53f0ba4deee7140cc1d326b58c474332efbad7ad
ijlee2 <ijlee2@users.noreply.github.com>
2024-12-23 14:39:20 +0100

    chore: Replaced ember-lts-5.4 and ember-lts-5.8 with ember-lts-5.12

--- tests/ember-intl/config/ember-try.js
+++ tests/ember-intl/config/ember-try.js
@@ -31,18 +31,10 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-lts-5.4',
+        name: 'ember-lts-5.12',
         npm: {
           devDependencies: {
-            'ember-source': '~5.4.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-5.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~5.8.0',
+            'ember-source': '~5.12.0',
           },
         },
       },
