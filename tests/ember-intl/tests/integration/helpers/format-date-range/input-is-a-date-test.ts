842e9ec7413528763c8e19bd542e355e5ea343f4
Miguel Andrade <jmandrade0@gmail.com>
2025-01-11 01:55:25 +0000

    remove date-time-range format docs

--- tests/ember-intl/tests/integration/helpers/format-date-range/input-is-a-date-test.ts
+++ tests/ember-intl/tests/integration/helpers/format-date-range/input-is-a-date-test.ts
@@ -53,10 +53,6 @@ module(
 
     test('we can format the date', async function (this: TestContext, assert) {
       await render<TestContext>(hbs`
-        <div data-test-output="1">
-          {{format-date-range this.from this.to format='custom'}}
-        </div>
-
         <div data-test-output="2">
           {{format-date-range
             this.from
@@ -67,10 +63,6 @@ module(
           }}
         </div>
       `);
-
-      assert
-        .dom('[data-test-output="1"]')
-        .hasText('1/23/14, 6:00 PM – 1/26/14, 7:30 PM');
 
       assert
         .dom('[data-test-output="2"]')
