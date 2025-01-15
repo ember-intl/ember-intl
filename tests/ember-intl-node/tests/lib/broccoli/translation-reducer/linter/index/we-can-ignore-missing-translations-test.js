5a5ddebee3357b0bf9c79ab2bc11b3c8aea66e1f
ijlee2 <ijlee2@users.noreply.github.com>
2025-01-15 12:08:33 +0100

    chore: Updated references (tests/ember-intl-node)

--- tests/ember-intl-node/tests/lib/broccoli/translation-reducer/linter/index/we-can-ignore-missing-translations-test.js
+++ tests/ember-intl-node/tests/lib/broccoli/translation-reducer/linter/index/we-can-ignore-missing-translations-test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import Linter from 'ember-intl/lib/broccoli/translation-reducer/linter/index.js';
+import Linter from 'ember-intl-v1-compat/lib/broccoli/translation-reducer/linter/index.js';
 
 describe('lib | broccoli | translation-reducer | linter | index', function () {
   it('we can ignore missing translations', function () {
--- tests/ember-intl-node/tests/lib/broccoli/translation-reducer/utils/strip-empty-translations/base-case-1-test.js
+++ tests/ember-intl-node/tests/lib/broccoli/translation-reducer/utils/strip-empty-translations/base-case-1-test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import stripEmptyTranslations from 'ember-intl/lib/broccoli/translation-reducer/utils/strip-empty-translations.js';
+import stripEmptyTranslations from 'ember-intl-v1-compat/lib/broccoli/translation-reducer/utils/strip-empty-translations.js';
 
 describe('lib | broccoli | translation-reducer | utils | strip-empty-translations', function () {
   it('base case (1)', function () {
