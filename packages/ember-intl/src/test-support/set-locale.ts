e057b14bc26afa87649a561659b6ed80fb34ec11
ijlee2 <ijlee2@users.noreply.github.com>
2025-01-15 12:07:42 +0100

    chore: Extracted test helpers

---
+++ packages/ember-intl/src/test-support/set-locale.ts
@@ -0,0 +1,19 @@
+import { getContext, settled, type TestContext } from '@ember/test-helpers';
+
+import type { IntlService } from '../index.ts';
+
+/**
+ * Updates the locale as if the user had changed their preferred language.
+ *
+ * @function setLocale
+ * @param {string} locale
+ */
+export async function setLocale(locale: string): Promise<void> {
+  const { owner } = getContext() as TestContext;
+
+  const intl = owner.lookup('service:intl') as IntlService;
+
+  intl.setLocale(locale);
+
+  await settled();
+}
