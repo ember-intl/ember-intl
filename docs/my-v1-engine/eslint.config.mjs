4788954720316bff782134e19b81cfda8d88d139
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-05 14:36:56 +0100

    chore: Updated project configurations

---
+++ docs/my-v1-engine/eslint.config.mjs
@@ -0,0 +1,4 @@
+import eslintConfigEmberV1Addon from '@shared-configs/eslint-config-ember/v1-addon/index.js';
+import eslintConfigYaml from '@shared-configs/eslint-config-yaml/index.js';
+
+export default [...eslintConfigEmberV1Addon, ...eslintConfigYaml];
---
+++ docs/my-v2-addon/eslint.config.mjs
@@ -0,0 +1,4 @@
+import eslintConfigEmberV2Addon from '@shared-configs/eslint-config-ember/v2-addon/index.js';
+import eslintConfigYaml from '@shared-configs/eslint-config-yaml/index.js';
+
+export default [...eslintConfigEmberV2Addon, ...eslintConfigYaml];
