8a246e7b490d851460e06cbd8cccfe7b4f39567f
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-04-03 13:41:09 +0200

    Removed inject as service (while still supporting ember-source@3.28) (#1960)
    * Cleanup `@ember/service` deprecation
    
    * chore: Removed inject as service from tests
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

---
+++ .changeset/hungry-singers-help.md
@@ -0,0 +1,6 @@
+---
+"ember-intl": patch
+"test-app-for-ember-intl": patch
+---
+
+Removed inject as service (while still supporting ember-source@3.28)
--- tests/ember-intl/app/controllers/smoke-tests.ts
+++ tests/ember-intl/app/controllers/smoke-tests.ts
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
+import * as s from '@ember/service';
 import type { IntlService } from 'ember-intl';
+
+const service = s.service ?? s.inject;
 
 export default class SmokeController extends Controller {
   @service declare intl: IntlService;
