27c4e391906472c1c56a9c83c342f03668bf8af1
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-06-19 08:07:11 +0200

    chore: Extracted test helpers

---
+++ packages/ember-intl/src/test-support/t.ts
@@ -0,0 +1,21 @@
+import { getContext, type TestContext } from '@ember/test-helpers';
+
+import type { IntlService } from '../index.ts';
+
+type TParameters = Parameters<IntlService['t']>;
+
+/**
+ * Invokes the `t` method of the `intl` service.
+ *
+ * @function t
+ * @param {string} key
+ * @param {object} [options]
+ * @return {string}
+ */
+export function t(key: TParameters[0], options?: TParameters[1]): string {
+  const { owner } = getContext() as TestContext;
+
+  const intl = owner.lookup('service:intl');
+
+  return intl.t(key, options);
+}
