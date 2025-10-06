58dc80e3c851b697be1711d07901d094529e5447
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-10-06 12:08:48 +0200

    chore: Installed @embroider/legacy-inspector-support

--- docs/ember-intl/app/app.ts
+++ docs/ember-intl/app/app.ts
@@ -1,11 +1,11 @@
-import './ember-inspector';
-
 import Application from '@ember/application';
+import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
 import config from 'docs-app-for-ember-intl/config/environment';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
 export default class App extends Application {
+  inspector = setupInspector(this);
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
