cb702bb7015f44d1007b33309e5f858e7fe5f63e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-10-23 17:28:38 +0200

    chore: Extracted test helpers

---
+++ packages/ember-intl/src/test-support.ts
@@ -0,0 +1,4 @@
+export { addTranslations } from './test-support/add-translations.ts';
+export { setLocale } from './test-support/set-locale.ts';
+export { setupIntl } from './test-support/setup-intl.ts';
+export { t } from './test-support/t.ts';
