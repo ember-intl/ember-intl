b64d4861ad1fce6053d36bbee7f537f75a492d5e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-10-23 17:28:32 +0200

    chore: Extracted helpers

---
+++ packages/ember-intl/src/helpers/format-date-range.ts
@@ -0,0 +1,28 @@
+import Helper from '@ember/component/helper';
+import { service } from '@ember/service';
+
+import type IntlService from '../services/intl.ts';
+
+type FormatParameters = Parameters<IntlService['formatDateRange']>;
+type From = FormatParameters[0];
+type To = FormatParameters[1];
+type Options = FormatParameters[2];
+
+interface FormatDateRangeSignature {
+  Args: {
+    Named?: Options;
+    Positional: [From, To];
+  };
+  Return: string;
+}
+
+export default class FormatDateRangeHelper extends Helper<FormatDateRangeSignature> {
+  @service declare intl: IntlService;
+
+  compute(
+    [from, to]: FormatDateRangeSignature['Args']['Positional'],
+    options: FormatDateRangeSignature['Args']['Named'],
+  ) {
+    return this.intl.formatDateRange(from, to, options);
+  }
+}
