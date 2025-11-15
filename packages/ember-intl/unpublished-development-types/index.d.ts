a4b55cb442cb015e508f8b8f31494585cc334124
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-11-15 08:31:58 +0100

    chore: Updated TypeScript configurations

--- packages/ember-intl/unpublished-development-types/index.d.ts
+++ packages/ember-intl/unpublished-development-types/index.d.ts
@@ -1,15 +1,3 @@
 // Add any types here that you need for local development only.
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
-
-import '@glint/environment-ember-loose';
-import '@glint/environment-ember-template-imports';
 import 'ember-qunit';
-
-import type EmberIntlRegistry from '../src/template-registry.ts';
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry extends EmberIntlRegistry {
-    // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
-    // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
-  }
-}
