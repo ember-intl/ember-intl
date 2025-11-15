8c35195aac22fee1cb31e895d825794ac18e8180
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-11-15 08:33:01 +0100

    chore: Updated Babel configurations

---
+++ packages/ember-intl/babel.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  generatorOpts: {
+    compact: false,
+  },
+  plugins: [
+    [
+      '@babel/plugin-transform-typescript',
+      {
+        allExtensions: true,
+        allowDeclareFields: true,
+        onlyRemoveTypeImports: true,
+      },
+    ],
+    '@embroider/addon-dev/template-colocation-plugin',
+    [
+      'babel-plugin-ember-template-compilation',
+      {
+        targetFormat: 'hbs',
+        transforms: [],
+      },
+    ],
+    [
+      'module:decorator-transforms',
+      {
+        runtime: {
+          import: 'decorator-transforms/runtime-esm',
+        },
+      },
+    ],
+  ],
+};
