cd2012e407de0bd81c589d78dd7ab6a4a8799253
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-01 11:53:46 -0600

    bugfix, refactor: Added tests (utils/lint-project)

---
+++ packages/ember-intl-lint/tests/utils/lint-project/find-missing-keys/some-keys-are-missing.test.ts
@@ -0,0 +1,35 @@
+import { assert, test } from '@codemod-utils/tests';
+
+import { findMissingKeys } from '../../../../src/utils/lint-project/index.js';
+
+test('utils | lint-project | find-missing-keys > some keys are missing', function () {
+  const project = {
+    availableKeys: new Map([
+      ['key01', ['translations/de-de.json']],
+      ['key02', ['translations/en-us.json']],
+      ['key06', ['translations/de-de.json']],
+      ['key07', ['translations/de-de.json', 'translations/en-us.json']],
+    ]),
+    usedKeys: new Map([
+      ['key03', ['app/components/file03.gts']],
+      ['key04', ['app/components/file02.gjs', 'app/components/file03.gts']],
+      [
+        'key05',
+        [
+          'app/components/file01.hbs',
+          'app/components/file02.gjs',
+          'app/components/file03.gts',
+        ],
+      ],
+      ['key06', ['app/templates/file01.hbs', 'app/templates/file02.gjs']],
+    ]),
+  };
+
+  const keys = findMissingKeys(project);
+
+  assert.deepStrictEqual(keys, [
+    'key03\n  - Found in app/components/file03.gts',
+    'key04\n  - Found in app/components/file02.gjs, app/components/file03.gts',
+    'key05\n  - Found in app/components/file01.hbs, app/components/file02.gjs, app/components/file03.gts',
+  ]);
+});
