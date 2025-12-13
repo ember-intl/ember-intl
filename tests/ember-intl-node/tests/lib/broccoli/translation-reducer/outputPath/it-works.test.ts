ee8ff65fb34da85cb4fe8044c925d109f53319ab
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-13 11:19:27 +0100

    chore: Converted tests (lib/broccoli/translation-reducer/outputPath)

---
+++ tests/ember-intl-node/tests/lib/broccoli/translation-reducer/outputPath/it-works.test.ts
@@ -0,0 +1,35 @@
+import { join } from 'node:path';
+
+import { assert, loadFixture, test } from '@codemod-utils/tests';
+import TranslationReducer from '@ember-intl/v1-compat/lib/broccoli/translation-reducer.js';
+
+import { buildTranslationsDir } from '../../../../helpers/index.js';
+
+test('lib | broccoli | translation-reducer | outputPath > it works', async function () {
+  const inputProject = {
+    'de-de.json': `{ "nested": { "key": "Hallo {name}!" }, "no-arguments": "Hallo Welt!" }`,
+    'en-us.json': `{ "nested": { "key": "Hello {name}!" }, "no-arguments": "Hello world!" }`,
+  };
+
+  const projectRoot = 'tmp/broccoli_merge_trees';
+  const inputPath = join(projectRoot);
+
+  loadFixture(inputProject, { projectRoot });
+
+  const outputNode = new TranslationReducer(inputPath, {
+    outputPath: 'custom/output/path',
+  });
+
+  const translationsDir = await buildTranslationsDir(outputNode);
+
+  assert.deepStrictEqual(translationsDir, {
+    custom: {
+      output: {
+        path: {
+          'de-de.json': `{"nested":{"key":"Hallo {name}!"},"no-arguments":"Hallo Welt!"}`,
+          'en-us.json': `{"nested":{"key":"Hello {name}!"},"no-arguments":"Hello world!"}`,
+        },
+      },
+    },
+  });
+});
