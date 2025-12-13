285326e0b5ba035944f96cce6c15a030a04d90b5
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-13 11:18:57 +0100

    chore: Converted tests (lib/utils/translation-reducer/get-translations)

---
+++ tests/ember-intl-node/tests/lib/utils/translation-reducer/get-translations/file-extension-is-json.test.ts
@@ -0,0 +1,27 @@
+import { join } from 'node:path';
+
+import { assert, loadFixture, test } from '@codemod-utils/tests';
+import getTranslations from '@ember-intl/v1-compat/lib/utils/translation-reducer/get-translations.js';
+
+test('lib | utils | translation-reducer | get-translations > file extension is .json', function () {
+  const inputProject = {
+    translations: {
+      'de-de.json': `{ "nested": { "key": "Hallo {name}!" }, "no-arguments": "Hallo Welt!" }`,
+      'en-us.json': `{ "nested": { "key": "Hello {name}!" }, "no-arguments": "Hello world!" }`,
+    },
+  };
+
+  const projectRoot = 'tmp/my-app';
+  const inputPath = join(projectRoot, 'translations');
+
+  loadFixture(inputProject, { projectRoot });
+
+  const output = getTranslations(join(inputPath, 'de-de.json'));
+
+  assert.deepStrictEqual(output, {
+    nested: {
+      key: 'Hallo {name}!',
+    },
+    'no-arguments': 'Hallo Welt!',
+  });
+});
