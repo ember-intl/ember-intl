5e1216fb925666c64c9df3ed6bcdb0ece92df52e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-01-10 09:35:46 +0100

    chore: Configured ember-intl for v2 apps

--- docs/my-v2-app-with-fallbacks/ember-intl.config.mjs
+++ docs/my-v2-app-with-fallbacks/ember-intl.config.mjs
@@ -1,5 +1,8 @@
 export default {
   addonPaths: ['node_modules/my-v1-addon', 'node_modules/my-v2-addon'],
+  buildOptions: {
+    fallbackLocale: 'en-us',
+  },
   lintRules: {
     'no-inconsistent-messages': {
       ignores: ['components.title', 'routes.index.title'],
--- docs/my-v2-app-with-lazy-loaded-translations/ember-intl.config.mjs
+++ docs/my-v2-app-with-lazy-loaded-translations/ember-intl.config.mjs
@@ -1,5 +1,8 @@
 export default {
   addonPaths: ['node_modules/my-v1-addon', 'node_modules/my-v2-addon'],
+  buildOptions: {
+    publicOnly: true,
+  },
   lintRules: {
     'no-missing-keys': {
       ignores: ['routes.index.key-without-translation'],
