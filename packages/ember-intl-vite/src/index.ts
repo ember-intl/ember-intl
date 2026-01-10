1548c6267ec8c50fdba4ccd88102b1050998e077
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-01-10 10:43:13 +0100

    reload

--- packages/ember-intl-vite/src/index.ts
+++ packages/ember-intl-vite/src/index.ts
@@ -1,13 +1,29 @@
 import { EOL } from 'node:os';
+import { join } from 'node:path';
 
 import type { Plugin } from 'vite';
 
 import { analyzeProject, createOptions } from './steps/index.js';
-import type { UserConfig } from './types/index.js';
+import type { Options, UserConfig } from './types/index.js';
+
+function getFoldersToWatch(options: Options): string[] {
+  const { config, projectRoot } = options;
+
+  const filePaths: string[] = [
+    // TODO: Vite/chokidar can't watch changes to `node_modules`
+    ...config.addonPaths.map((addonPath) => {
+      return join(projectRoot, addonPath, 'translations');
+    }),
+    join(projectRoot, 'translations'),
+  ];
+
+  return filePaths;
+}
 
 export function loadTranslations(userConfig?: UserConfig): Plugin {
   const virtualModuleId = 'virtual:ember-intl/translations' as const;
   const resolvedVirtualModuleIds = new Set<string>();
+  let _options: Options | undefined;
 
   return {
     name: 'ember-intl-load-translations',
@@ -29,6 +45,8 @@ export function loadTranslations(userConfig?: UserConfig): Plugin {
       }
 
       const options = createOptions(userConfig);
+      _options = options;
+
       const { translations } = analyzeProject(options);
       const locale = id
         .replace(`\0${virtualModuleId}/`, '')
@@ -41,6 +59,37 @@ export function loadTranslations(userConfig?: UserConfig): Plugin {
         ``,
         `export default translations;`,
       ].join(EOL);
+    },
+
+    async handleHotUpdate({ file: filePath, server }) {
+      if (!_options) {
+        return;
+      }
+
+      const foldersToWatch = getFoldersToWatch(_options);
+
+      const isTranslation = foldersToWatch.some((folder) => {
+        return filePath.startsWith(folder);
+      });
+
+      if (!isTranslation) {
+        return;
+      }
+
+      const promises = Array.from(resolvedVirtualModuleIds).reduce(
+        (accumulator, id) => {
+          const module = server.moduleGraph.getModuleById(id);
+
+          if (module) {
+            accumulator.push(server.reloadModule(module));
+          }
+
+          return accumulator;
+        },
+        [] as Promise<void>[],
+      );
+
+      await Promise.allSettled(promises);
     },
   };
 }
