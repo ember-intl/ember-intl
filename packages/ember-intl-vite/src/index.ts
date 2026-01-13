237bd502767973adf156884f304ae3bc63ddc285
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-01-13 07:39:11 +0100

    feature: Scaffolded resolveId and load hooks

--- packages/ember-intl-vite/src/index.ts
+++ packages/ember-intl-vite/src/index.ts
@@ -1,1 +1,31 @@
-export { loadTranslations } from './load-translations.js';
+import { EOL } from 'node:os';
+
+import type { Plugin } from 'vite';
+
+import { getLocale, resolveModuleId } from './utils/vite.js';
+
+export function loadTranslations(): Plugin {
+  return {
+    name: 'ember-intl-load-translations',
+
+    resolveId(id) {
+      return resolveModuleId(id);
+    },
+
+    load(id) {
+      const locale = getLocale(id);
+
+      if (locale === undefined) {
+        return;
+      }
+
+      const translations = {};
+
+      return [
+        `const translations = ${JSON.stringify(translations)};`,
+        ``,
+        `export default translations;`,
+      ].join(EOL);
+    },
+  };
+}
--- packages/ember-intl-vite/src/types/index.ts
+++ packages/ember-intl-vite/src/types/index.ts
@@ -1,9 +1,25 @@
-export type Options = Partial<{
+type BuildOptions = {
+  fallbackLocale: string | undefined;
+  inputPath: string;
+  publicOnly: boolean;
+  wrapTranslationsWithNamespace: boolean;
+};
+
+type Config = {
   addonPaths: string[];
-  buildOptions: Partial<{
-    fallbackLocale: string;
-    inputPath: string;
-    publicOnly: boolean;
-    wrapTranslationsWithNamespace: boolean;
-  }>;
+  buildOptions: BuildOptions;
+};
+
+type Locale = string;
+
+type Options = {
+  config: Config;
+  projectRoot: string;
+};
+
+type UserConfig = Partial<{
+  addonPaths: string[];
+  buildOptions: Partial<BuildOptions>;
 }>;
+
+export type { Config, Locale, Options, UserConfig };
