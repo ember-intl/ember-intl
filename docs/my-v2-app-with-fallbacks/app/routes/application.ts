2dde410f07dc1ec6d434e6986a1460a6f49521ba
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-01-13 18:44:38 +0100

    chore: Updated project configurations

--- docs/my-v2-app-with-fallbacks/app/routes/application.ts
+++ docs/my-v2-app-with-fallbacks/app/routes/application.ts
@@ -2,9 +2,7 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import type { IntlService } from 'ember-intl';
-// @ts-expect-error: Incorrect type
 import translationsForDeDe from 'virtual:ember-intl/translations/de-de';
-// @ts-expect-error: Incorrect type
 import translationsForEnUs from 'virtual:ember-intl/translations/en-us';
 
 export default class ApplicationRoute extends Route {
@@ -15,9 +13,7 @@ export default class ApplicationRoute extends Route {
   }
 
   private setupIntl(): void {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     this.intl.addTranslations('de-de', translationsForDeDe);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     this.intl.addTranslations('en-us', translationsForEnUs);
 
     this.intl.setLocale(['de-de', 'en-us']);
