1fb7731ab1ad8871b3b5af82019e075c4a8ce188
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-01-24 10:33:27 +0100

    refactor: Extracted function (@ember-intl/lint)

--- packages/ember-intl-lint/src/steps/analyze-project/find-translation-files.ts
+++ packages/ember-intl-lint/src/steps/analyze-project/find-translation-files.ts
@@ -4,48 +4,49 @@ import { findFiles, parseFilePath } from '@codemod-utils/files';
 
 import type { Options, Project } from '../../types/index.js';
 
+function getPattern(translationsDir: string): string {
+  return join(translationsDir, '**/*.{json,yaml,yml}').replaceAll(sep, '/');
+}
+
 export function findTranslationFiles(
   options: Options,
 ): Project['translationFiles'] {
   const { config, projectRoot } = options;
+  const { addonPaths, buildOptions } = config;
+
   const translationFiles: Project['translationFiles'] = new Map();
 
-  config.addonPaths.forEach((addonPath) => {
+  addonPaths.forEach((addonPath) => {
     const translationsDir = join(addonPath, 'translations');
 
-    const filePaths = findFiles(
-      join(translationsDir, '**/*.{json,yaml,yml}').replaceAll(sep, '/'),
-      {
-        projectRoot,
-      },
-    );
+    const filePaths = findFiles(getPattern(translationsDir), {
+      projectRoot,
+    });
 
     filePaths.forEach((filePath) => {
-      const { name: locale } = parseFilePath(filePath);
+      const { name } = parseFilePath(filePath);
 
       translationFiles.set(filePath, {
         isInternal: false,
-        locale,
+        locale: name,
         translationsDir,
       });
     });
   });
 
-  const translationsDir = normalize(config.buildOptions.inputPath);
+  // App's translations take precedence
+  const translationsDir = normalize(buildOptions.inputPath);
 
-  const filePaths = findFiles(
-    join(translationsDir, '**/*.{json,yaml,yml}').replaceAll(sep, '/'),
-    {
-      projectRoot,
-    },
-  );
+  const filePaths = findFiles(getPattern(translationsDir), {
+    projectRoot,
+  });
 
   filePaths.forEach((filePath) => {
-    const { name: locale } = parseFilePath(filePath);
+    const { name } = parseFilePath(filePath);
 
     translationFiles.set(filePath, {
       isInternal: true,
-      locale,
+      locale: name,
       translationsDir,
     });
   });
