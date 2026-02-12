c85a01e067e59c4bd3ead3164d6c50888e71913d
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-02-12 10:43:29 +0100

    Update configurations for docs-app-for-ember-intl (#2103)
    * chore: Updated configurations
    
    * chore: Added changeset

--- docs/ember-intl/.vitepress/theme/index.ts
+++ docs/ember-intl/.vitepress/theme/index.ts
@@ -1,7 +1,8 @@
 import './custom-styles.css';
 
 import VitepressTheme from 'vitepress/theme';
+import type { Theme } from 'vitepress';
 
 export default {
   extends: VitepressTheme,
-};
+} satisfies Theme;
