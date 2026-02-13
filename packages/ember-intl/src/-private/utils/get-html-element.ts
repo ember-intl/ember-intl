54486ea8526d2cb1b4d20a0cdc913ad65b6898db
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-02-13 06:48:40 +0100

    chore: Removed deprecated syntax

--- packages/ember-intl/src/-private/utils/get-html-element.ts
+++ packages/ember-intl/src/-private/utils/get-html-element.ts
@@ -1,4 +1,4 @@
-import { getOwner } from '@ember/application';
+import { getOwner } from '@ember/owner';
 
 type SimpleDocument = {
   documentElement: HTMLHtmlElement;
--- tests/ember-intl/tests/integration/helpers/format-number/input-is-a-currency-test.gts
+++ tests/ember-intl/tests/integration/helpers/format-number/input-is-a-currency-test.gts
@@ -1,4 +1,4 @@
-import { setOwner } from '@ember/application';
+import { setOwner } from '@ember/owner';
 import {
   render,
   type TestContext as BaseTestContext,
