3dd9df0fa899e18734d03dc1cf234ae6c69deb61
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-02-13 06:47:49 +0100

    bugfix, refactor: Removed outdated assumptions for updating the lang attribute

---
+++ packages/ember-intl/src/-private/utils/get-html-element.ts
@@ -0,0 +1,23 @@
+import { getOwner } from '@ember/application';
+
+type SimpleDocument = {
+  documentElement: HTMLHtmlElement;
+};
+
+/**
+ * @private
+ * @hide
+ */
+export function getHtmlElement(context: object): HTMLHtmlElement | undefined {
+  const owner = getOwner(context);
+
+  if (owner === undefined) {
+    return undefined;
+  }
+
+  const documentService = owner.lookup('service:-document') as
+    | SimpleDocument
+    | undefined;
+
+  return documentService?.documentElement;
+}
