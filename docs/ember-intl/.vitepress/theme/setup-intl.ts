f023e4de715a246f910f56a1c4a288f288d17419
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-02-13 08:04:54 +0100

    chore: Defined translations and formats

--- docs/ember-intl/.vitepress/theme/setup-intl.ts
+++ docs/ember-intl/.vitepress/theme/setup-intl.ts
@@ -1,9 +1,67 @@
 import type { Registry as Services } from '@ember/service';
 import IntlService from 'ember-intl/services/intl';
+import translationsForDeDe from 'virtual:ember-intl/translations/de-de';
+import translationsForEnUs from 'virtual:ember-intl/translations/en-us';
+import translationsForEsEs from 'virtual:ember-intl/translations/es-es';
+import translationsForFrFr from 'virtual:ember-intl/translations/fr-fr';
+import type { Formats } from 'ember-intl';
+
+export const formats: Formats = {
+  formatDate: {
+    'user-friendly': {
+      day: 'numeric',
+      month: 'long',
+      weekday: 'short',
+    },
+  },
+
+  formatDateRange: {
+    'user-friendly': {
+      day: 'numeric',
+      month: 'short',
+    },
+  },
+
+  formatNumber: {
+    compact: {
+      notation: 'compact',
+    },
+    EUR: {
+      currency: 'EUR',
+      style: 'currency',
+    },
+    USD: {
+      currency: 'USD',
+      style: 'currency',
+    },
+  },
+
+  formatRelativeTime: {
+    compact: {
+      style: 'narrow',
+    },
+  },
+
+  formatTime: {
+    hhmmss: {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    },
+  },
+};
 
 export function setupIntl(): Services['intl'] {
   // @ts-expect-error: This expression is not constructable.
   const intl = new IntlService() as Services['intl'];
+
+  intl.addTranslations('de-de', translationsForDeDe);
+  intl.addTranslations('en-us', translationsForEnUs);
+  intl.addTranslations('es-es', translationsForEsEs);
+  intl.addTranslations('fr-fr', translationsForFrFr);
+
+  intl.setFormats(formats);
+  intl.setLocale(['en-us']);
 
   return intl;
 }
