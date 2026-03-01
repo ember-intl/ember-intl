5fd651c3e286f45b17237732b0570eaca6c71fb2
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-01 10:19:23 +0100

    refactor: Restricted API

--- packages/ember-intl-lint/src/utils/worker/index.ts
+++ packages/ember-intl-lint/src/utils/worker/index.ts
@@ -1,3 +1,2 @@
-export * from './create-run-worker.js';
 export * from './parallelize.js';
 export * from './run-task.js';
--- packages/ember-intl-lint/src/utils/worker/parallelize.ts
+++ packages/ember-intl-lint/src/utils/worker/parallelize.ts
@@ -1,6 +1,6 @@
 import { availableParallelism } from 'node:os';
+import { SHARE_ENV, Worker } from 'node:worker_threads';
 
-import { createRunWorker } from './create-run-worker.js';
 import { runTask, type Task } from './run-task.js';
 
 const MIN_NUM_TASKS_PER_WORKER = 100;
@@ -19,6 +19,33 @@ function batchDatas<T>(
   const mainThreadData: T[] = workerDatas.shift() ?? [];
 
   return [mainThreadData, workerDatas];
+}
+
+function createRunWorker<U>(
+  workerFilePath: string,
+): <T>(taskDatas: T[]) => Promise<U[]> {
+  function runWorker<T>(taskDatas: T[]): Promise<U[]> {
+    return new Promise((resolve, reject) => {
+      const workerUrl = new URL(workerFilePath, import.meta.url);
+
+      const worker = new Worker(workerUrl, {
+        env: SHARE_ENV,
+        workerData: {
+          taskDatas,
+        },
+      });
+
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`));
+        }
+      });
+    });
+  }
+
+  return runWorker;
 }
 
 export async function parallelize<T extends unknown[], U>(
