cf831ef0934fe596dab1102bccfc6da794004465
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-06 10:07:54 +0100

    bugfix: Patched types from formatjs

--- packages/ember-intl/src/-private/formatjs/format-display-name.ts
+++ packages/ember-intl/src/-private/formatjs/format-display-name.ts
@@ -1,12 +1,29 @@
 import type { IntlShape } from '@formatjs/intl';
 
-export type FormatDisplayNameParameters = Parameters<
-  IntlShape<string>['formatDisplayName']
->;
+type FormatjsParameters = Parameters<IntlShape<string>['formatDisplayName']>;
+
+export type FormatDisplayNameParameters = [
+  FormatjsParameters[0],
+  (
+    | {
+        fallback?: FormatjsParameters[1]['fallback'];
+        languageDisplay?: FormatjsParameters[1]['languageDisplay'];
+        locale?: string;
+        style?: FormatjsParameters[1]['style'];
+        type: 'language';
+      }
+    | {
+        fallback?: FormatjsParameters[1]['fallback'];
+        locale?: string;
+        style?: FormatjsParameters[1]['style'];
+        type: Exclude<FormatjsParameters[1]['type'], 'language'>;
+      }
+  ),
+];
 
 export function formatDisplayName(
   intlShape: IntlShape,
   ...[value, formatOptions]: FormatDisplayNameParameters
 ): string {
-  return intlShape.formatDisplayName(value, formatOptions);
+  return intlShape.formatDisplayName(value, formatOptions) ?? '';
 }
