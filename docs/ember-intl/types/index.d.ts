a6ad63e08a893169cca6981c9714a81db9b50b74
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-16 09:05:28 +0100

    chore: Updated project configurations

--- docs/ember-intl/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'vite-plugin-ember/components/code-preview.vue' {
-  import type { DefineComponent } from 'vue';
-
-  const component: DefineComponent<object, object, unknown>;
-
-  export default component;
-}
