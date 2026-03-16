61ce192015c6901c035db94fa74de8f36b85a7fd
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-16 13:13:48 +0100

    Updated dependencies (#2125)
    * chore: Updated dependencies
    
    * chore: Added lockfile
    
    * chore: Addressed breaking changes
    
    * chore: Added changesets

---
+++ .changeset/quick-shirts-reply.md
@@ -0,0 +1,5 @@
+---
+"@ember-intl/vite": patch
+---
+
+Began testing with vite@8.0.0
--- packages/ember-intl-lint/src/steps/analyze-project/find-used-keys/worker.ts
+++ packages/ember-intl-lint/src/steps/analyze-project/find-used-keys/worker.ts
@@ -1,19 +1,5 @@
-import { parentPort, workerData } from 'node:worker_threads';
-
-import { runTask } from '@codemod-utils/threads';
+import { runWorker } from '@codemod-utils/threads';
 
 import { task } from './task.js';
 
-type WorkerData = {
-  datasets: Parameters<typeof task>[];
-};
-
-const { datasets } = workerData as WorkerData;
-
-runTask(task, datasets)
-  .then((result) => {
-    parentPort?.postMessage(result);
-  })
-  .catch((error) => {
-    throw error;
-  });
+runWorker(task);
