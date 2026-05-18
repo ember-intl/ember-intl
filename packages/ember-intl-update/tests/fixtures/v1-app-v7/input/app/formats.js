0c154893f34ec7f364caf511a06752a2196582bb
ijlee2 <ijlee2@users.noreply.github.com>
2026-05-18 20:16:42 +0200

    chore: Updated fixtures

---
+++ packages/ember-intl-update/tests/fixtures/v1-app-v7/input/app/formats.js
@@ -0,0 +1,31 @@
+export default {
+  date: {
+    'user-friendly': {
+      day: 'numeric',
+      month: 'long',
+      weekday: 'short',
+    },
+  },
+
+  number: {
+    compact: {
+      notation: 'compact',
+    },
+    EUR: {
+      currency: 'EUR',
+      style: 'currency',
+    },
+    USD: {
+      currency: 'USD',
+      style: 'currency',
+    },
+  },
+
+  time: {
+    hhmmss: {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    },
+  },
+};
