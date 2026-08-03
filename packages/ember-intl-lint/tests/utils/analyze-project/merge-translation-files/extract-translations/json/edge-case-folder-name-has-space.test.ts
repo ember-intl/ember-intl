a9db28d46db7ee9c790ec87cddaf65ff6ebb3786
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-08-03 19:08:28 +0200

    Removed handling spaces in translation folder names (#2184)
    * breaking: Removed handling spaces in translation folder names (@ember-intl/lint)
    
    * breaking: Removed handling spaces in translation folder names (@ember-intl/v1-compat)
    
    * breaking: Removed handling spaces in translation folder names (@ember-intl/vite)
    
    * chore: Documented removing handling spaces in translation folder names
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/ember-intl-lint/tests/utils/analyze-project/merge-translation-files/extract-translations/json/edge-case-folder-name-has-space.test.ts
+++ packages/ember-intl-lint/tests/utils/analyze-project/merge-translation-files/extract-translations/json/edge-case-folder-name-has-space.test.ts
@@ -22,16 +22,16 @@ test('utils | analyze-project | merge-translation-files | extract-translations |
   });
 
   assert.deepStrictEqual(translationObject, {
-    '__things_for__product_.card.learn-more.aria-label':
+    '  things for  product .card.learn-more.aria-label':
       'Learn more about {productName}',
-    '__things_for__product_.card.learn-more.label': 'Learn more',
-    '__things_for__product_.details.add-to-cart': 'Add to Cart',
-    '__things_for__product_.details.description': 'Description',
-    '__things_for__product_.details.price': 'Price',
-    '__things_for__product_.details.rating': 'Rating',
-    '__things_for__product_.details.rating-value':
+    '  things for  product .card.learn-more.label': 'Learn more',
+    '  things for  product .details.add-to-cart': 'Add to Cart',
+    '  things for  product .details.description': 'Description',
+    '  things for  product .details.price': 'Price',
+    '  things for  product .details.rating': 'Rating',
+    '  things for  product .details.rating-value':
       '{productRating} out of 5 stars',
-    '__things_for__product_.details.seller': 'Seller',
-    '__things_for__product_.title': '{productName}',
+    '  things for  product .details.seller': 'Seller',
+    '  things for  product .title': '{productName}',
   });
 });
